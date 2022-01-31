3317efbe872670ba9fb9d911ef325b645d6cb703
Bruno Salmon <dev.salmonb@gmail.com>
2022-01-31 22:52:39 +0000

    Added options activity in backend application (= editable frontend options = potential WYSIWYG event editor)

--- mongoose-backend/mongoose-backend-application/src/main/java/module-info.java
+++ mongoose-backend/mongoose-backend-application/src/main/java/module-info.java
@@ -14,6 +14,7 @@ module mongoose.backend.application {
     requires mongoose.backend.activities.letters;
     requires mongoose.backend.activities.monitor;
     requires mongoose.backend.activities.operations;
+    requires mongoose.backend.activities.options;
     requires mongoose.backend.activities.organizations;
     requires mongoose.backend.activities.payments;
     requires mongoose.backend.activities.roomsgraphic;
