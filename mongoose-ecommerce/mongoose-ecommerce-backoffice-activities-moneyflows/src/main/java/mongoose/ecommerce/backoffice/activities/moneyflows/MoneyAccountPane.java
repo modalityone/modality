ad57fb0599af503c06c56357aadfcf0824c36ef3
goatsandtigers <danielnewman106@hotmail.com>
2022-03-19 09:36:22 +0000

    Selected MoneyAccount is highlighted.

--- mongoose-ecommerce/mongoose-ecommerce-backoffice-activities-moneyflows/src/main/java/mongoose/ecommerce/backoffice/activities/moneyflows/MoneyAccountPane.java
+++ mongoose-ecommerce/mongoose-ecommerce-backoffice-activities-moneyflows/src/main/java/mongoose/ecommerce/backoffice/activities/moneyflows/MoneyAccountPane.java
@@ -2,6 +2,7 @@ package mongoose.ecommerce.backoffice.activities.moneyflows;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ObservableObjectValue;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
@@ -15,9 +16,12 @@ public class MoneyAccountPane extends HBox {
 
     private final ObjectProperty<MoneyAccount> moneyAccountProperty = new SimpleObjectProperty<>();
     public ObjectProperty<MoneyAccount> moneyAccountProperty() { return moneyAccountProperty; }
+    private final ObservableObjectValue<MoneyAccount> selectedMoneyAccount;
     private final Label label = new Label();
 
-    public MoneyAccountPane(MoneyAccount moneyAccount) {
+    public MoneyAccountPane(MoneyAccount moneyAccount, ObservableObjectValue<MoneyAccount> selectedMoneyAccount) {
+        this.selectedMoneyAccount = selectedMoneyAccount;
+        selectedMoneyAccount.addListener(e -> updateBorder());
         moneyAccountProperty.addListener(e -> label.setText(moneyAccountProperty.get().getName()));
         moneyAccountProperty.set(moneyAccount);
         setPadding(new Insets(8));
@@ -27,12 +31,13 @@ public class MoneyAccountPane extends HBox {
     }
 
     private void updateBorder() {
+        String color = moneyAccountProperty.get().equals(selectedMoneyAccount.get()) ? "yellow" : "lightgray";
         setStyle("-fx-padding: 10;" +
                 "-fx-border-style: solid inside;" +
                 "-fx-border-width: 2;" +
                 "-fx-border-insets: 5;" +
                 "-fx-border-radius: 5;" +
-                "-fx-border-color: lightgray;");
+                "-fx-border-color: " + color + ";");
     }
 
 }
