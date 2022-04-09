7ab934cf4c3a4000e99777fe944638cf104549e1
goatsandtigers <danielnewman106@hotmail.com>
2022-04-09 12:07:35 +0100

    Replaced usage of setStyle() because not supported by WebFX.

--- mongoose-ecommerce/mongoose-ecommerce-backoffice-activities-moneyflows/src/main/java/mongoose/ecommerce/backoffice/activities/moneyflows/MoneyAccountEditorPane.java
+++ mongoose-ecommerce/mongoose-ecommerce-backoffice-activities-moneyflows/src/main/java/mongoose/ecommerce/backoffice/activities/moneyflows/MoneyAccountEditorPane.java
@@ -2,10 +2,11 @@ package mongoose.ecommerce.backoffice.activities.moneyflows;
 
 import dev.webfx.framework.client.ui.controls.entity.sheet.EntityPropertiesSheet;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import mongoose.base.shared.entities.MoneyAccount;
@@ -23,7 +24,7 @@ public class MoneyAccountEditorPane extends VBox {
 
     public MoneyAccountEditorPane(ObservableList<MoneyAccountPane> moneyAccountPanes, ObservableList<MoneyFlowArrowView> moneyFlowArrowViews) {
         setPrefWidth(400);
-        setStyle("-fx-background-color: lightgray;");
+        setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, CornerRadii.EMPTY, Insets.EMPTY)));
         fromMoneyAccountListGrid = new MoneyAccountFromListGrid(moneyAccountPanes, moneyFlowArrowViews);
         toMoneyAccountListGrid = new MoneyAccountToListGrid(moneyAccountPanes, moneyFlowArrowViews);
         VBox gridVbox = new VBox(
