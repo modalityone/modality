15f0c661c5ee91f8785ffcbcdf7ea41226c353b7
Bruno Salmon <dev.salmonb@gmail.com>
2022-04-19 16:36:40 +0100

    Re-established default backoffice start route (changed unintentionally)

--- mongoose-all/mongoose-all-backoffice-application/src/main/java/mongoose/all/backoffice/application/MongooseBackOfficeActivity.java
+++ mongoose-all/mongoose-all-backoffice-application/src/main/java/mongoose/all/backoffice/application/MongooseBackOfficeActivity.java
@@ -9,7 +9,7 @@ import mongoose.base.client.application.MongooseClientActivity;
  */
 final class MongooseBackOfficeActivity extends MongooseClientActivity {
 
-    private static final String DEFAULT_START_PATH = "/money-flows/organization/151";
+    private static final String DEFAULT_START_PATH = "/organizations";
 
     MongooseBackOfficeActivity() {
         super(DEFAULT_START_PATH);
