8903e8bf6f0f9f6d3fea4192056cbed49a96a88f
Bruno Salmon <dev.salmonb@gmail.com>
2022-04-19 10:28:03 +0100

    Updated module files from webfx build tool

--- mongoose-ecommerce/mongoose-ecommerce-backoffice-operations-moneyflow/src/main/java/module-info.java
+++ mongoose-ecommerce/mongoose-ecommerce-backoffice-operations-moneyflow/src/main/java/module-info.java
@@ -3,14 +3,15 @@
 module mongoose.ecommerce.backoffice.operations.moneyflow {
 
     // Direct dependencies modules
+    requires java.base;
     requires javafx.graphics;
     requires mongoose.base.shared.entities;
+    requires webfx.framework.client.controls;
     requires webfx.framework.client.orm.entity.controls;
     requires webfx.framework.shared.operation;
     requires webfx.framework.shared.orm.entity;
-    requires webfx.platform.shared.util;
     requires webfx.platform.shared.submit;
-    requires webfx.framework.client.controls;
+    requires webfx.platform.shared.util;
 
     // Exported packages
     exports mongoose.ecommerce.backoffice.operations.entities.moneyaccount;
