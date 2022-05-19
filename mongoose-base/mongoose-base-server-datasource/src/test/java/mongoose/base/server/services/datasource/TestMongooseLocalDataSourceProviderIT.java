5a76b18c22d23c32c3bb5ef1250c0e665c141d75
andreww51 <andrew@jellystone.org>
2022-05-19 15:12:35 +0100

    Kbs 106 vary db host (#6)
    * KBS-106: Override db host. Add unit test deps. Add unit test structure and a first test. Add Dockerfiles for building server and back office images. Add powershell build script. Add build notes.
    
    * KBS-106: Remove workflow experiment.
    
    * KBS-106: Vary DB Host. Add unit/int tests. Add docker for mongoose. Add Build notes. Remove broken workflow. Add windows build script.
    
    * KBS-106: Vary DB Host. Add unit/int tests. Add docker for mongoose. Add Build notes. Remove broken workflow. Add windows build script.
    
    * KBS-106: Remove unneeded change.
    
    * KBS-106: Selenium experiment.
    
    * KBS-106: Selenium experiment.

--- mongoose-base/mongoose-base-server-datasource/src/test/java/mongoose/base/server/services/datasource/TestMongooseLocalDataSourceProviderIT.java
+++ mongoose-base/mongoose-base-server-datasource/src/test/java/mongoose/base/server/services/datasource/TestMongooseLocalDataSourceProviderIT.java
@@ -1,15 +1,28 @@
 package mongoose.base.server.services.datasource;
 
+<<<<<<< HEAD
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mockStatic;
 
 public class TestMongooseLocalDataSourceProviderIT {
 
+    private static final String HOME = "http://localhost:8082/mongoose-all-backoffice-application-gwt-1.0.0-SNAPSHOT/mongoose_all_backoffice_application_gwt/";
+
     @Test
-    public void loadsPropertyFromEnvironment() {
-        assertEquals(1, 1);
+    @DisplayName("The home page has the expected title.")
+    public void hasExpectedTitle() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get(HOME);
+        String title = driver.getTitle();
+        assertEquals("Mongoose Back-Office", title);
     }
 
 }
