bdfec2a051890d52060ed15288e1a5f3be7ab08a
Ben <ben@Eden.local>
2022-05-24 12:22:06 +0100

    fixed the gridlayout to at 50% each main table.

--- mongoose-base/mongoose-base-backoffice-activities-filters/src/main/java/mongoose/base/backoffice/activities/filters/FiltersActivity.java
+++ mongoose-base/mongoose-base-backoffice-activities-filters/src/main/java/mongoose/base/backoffice/activities/filters/FiltersActivity.java
@@ -11,9 +11,12 @@ import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.paint.Color;
+import javafx.scene.layout.Priority;
+import mongoose.base.shared.entities.Filter;
 import mongoose.base.client.presentationmodel.HasSearchTextProperty;
-import mongoose.base.shared.entities.MoneyAccount;
 import mongoose.crm.backoffice.controls.bookingdetailspanel.BookingDetailsPanel;
 import mongoose.base.backoffice.controls.masterslave.ConventionalUiBuilderMixin;
 import mongoose.base.backoffice.operations.entities.generic.CopySelectionRequest;
@@ -40,20 +43,17 @@ final class FiltersActivity extends EventDependentViewDomainActivity implements 
     @Override
     public FiltersPresentationModel getPresentationModel() { return pm; }
 
-    private ReactiveVisualMapper<MoneyAccount> filtersVisualMapper;
-
     @Override
     public Node buildUi() {
 
         // FilterPane components
-        Label searchLabel = new Label("Search filters");
+        Label filterSearchLabel = new Label("Select search filter");
         TextField filterSearchField = new TextField();
         ((HasSearchTextProperty) pm).searchTextProperty().bind(filterSearchField.textProperty());
 
         VisualGrid filterGrid = new VisualGrid();
         filterGrid.visualResultProperty().bind(pm.filtersVisualResultProperty());
 
-        // The FilterPane button bar
         HBox filterPaneButtonBar = new HBox();
         filterPaneButtonBar.getChildren().add(new Button("Add"));
         filterPaneButtonBar.getChildren().add(new Button("Edit"));
@@ -61,20 +61,61 @@ final class FiltersActivity extends EventDependentViewDomainActivity implements 
         filterPaneButtonBar.setSpacing(5);
         filterPaneButtonBar.setAlignment(Pos.BASELINE_RIGHT);
 
-        // The FilterPane container of components
         VBox filterPane = new VBox();
-        filterPane.getChildren().add(searchLabel);
+        filterPane.getChildren().add(filterSearchLabel);
         filterPane.getChildren().add(filterSearchField);
         filterPane.getChildren().add(filterGrid);
         filterPane.getChildren().add(filterPaneButtonBar);
         filterPane.setSpacing(10);
         VBox.setMargin(filterPane, new Insets(20, 20, 20, 20));
 
-        // The FilterPane border wrapper
-        VBox filterPaneBorder = new VBox();
-        filterPaneBorder.getChildren().add(filterPane);
-        filterPaneBorder.setBorder(new Border(new BorderStroke(Color.LIGHTGRAY, BorderStrokeStyle.SOLID, null, null)));
-        VBox.setMargin(filterPaneBorder, new Insets(20, 20, 20, 20));
+
+        // FieldPane components
+        Label fieldSearchLabel = new Label("Search fields");
+        TextField fieldSearchField = new TextField();
+        //((HasSearchTextProperty) pm).searchTextProperty().bind(fieldSearchField.textProperty());
+
+        VisualGrid fieldGrid = new VisualGrid();
+        fieldGrid.visualResultProperty().bind(pm.fieldsVisualResultProperty());
+
+        // The FieldPane button bar
+        HBox fieldPaneButtonBar = new HBox();
+        fieldPaneButtonBar.getChildren().add(new Button("Add"));
+        fieldPaneButtonBar.getChildren().add(new Button("Edit"));
+        fieldPaneButtonBar.getChildren().add(new Button("Delete"));
+        fieldPaneButtonBar.setSpacing(5);
+        fieldPaneButtonBar.setAlignment(Pos.BASELINE_RIGHT);
+
+        // The FieldPane container of components
+        VBox fieldPane = new VBox();
+        fieldPane.getChildren().add(fieldSearchLabel);
+        fieldPane.getChildren().add(fieldSearchField);
+        fieldPane.getChildren().add(fieldGrid);
+        fieldPane.getChildren().add(fieldPaneButtonBar);
+        fieldPane.setSpacing(10);
+        VBox.setMargin(fieldPane, new Insets(20, 20, 20, 20));
+
+
+        // Place the FilterPane and FieldPane side-by-side
+        GridPane filterAndFieldPanes = new GridPane();
+        ColumnConstraints leftColumn = new ColumnConstraints();
+        leftColumn.setPercentWidth(50);
+
+        ColumnConstraints rightColumn = new ColumnConstraints();
+        rightColumn.setPercentWidth(50);
+
+        filterAndFieldPanes.getColumnConstraints().addAll(leftColumn,rightColumn);
+        filterAndFieldPanes.setHgap(30);
+        filterAndFieldPanes.add(filterPane, 0, 0);
+        filterAndFieldPanes.add(fieldPane, 1, 0);
+
+
+        // Place the filterAndFieldPanes in a border wrapper
+        VBox filterAndFieldPaneBorder = new VBox();
+        filterAndFieldPaneBorder.getChildren().add(filterAndFieldPanes);
+        filterAndFieldPaneBorder.setBorder(new Border(new BorderStroke(Color.LIGHTGRAY, BorderStrokeStyle.SOLID, null, null)));
+        VBox.setMargin(filterAndFieldPaneBorder, new Insets(20, 20, 20, 20));
+
 
         // ResultPane components
         Label resultLabel = new Label("Returned results");
@@ -95,7 +136,7 @@ final class FiltersActivity extends EventDependentViewDomainActivity implements 
 
         // The outer box of components.
         VBox outerVerticalBox = new VBox();
-        outerVerticalBox.getChildren().add(filterPaneBorder);
+        outerVerticalBox.getChildren().add(filterAndFieldPaneBorder);
         outerVerticalBox.getChildren().add(resultPaneBorder);
         return outerVerticalBox;
     }
@@ -111,7 +152,8 @@ final class FiltersActivity extends EventDependentViewDomainActivity implements 
     =================================================== Logical layer ==================================================
     ==================================================================================================================*/
 
-    private ReactiveVisualMapper<Document> groupVisualMapper, masterVisualMapper;
+    private ReactiveVisualMapper<Filter> filtersVisualMapper;
+    private ReactiveVisualMapper<Filter> fieldsVisualMapper;
 
     @Override
     protected void startLogic() {
@@ -141,14 +183,53 @@ final class FiltersActivity extends EventDependentViewDomainActivity implements 
         */
 
         // Setting up the filter mapper that builds the content displayed in the filters listing
-        filtersVisualMapper = ReactiveVisualMapper.<MoneyAccount>createPushReactiveChain(this)
-                .always("{class: 'Filter', alias: 'fil', columns: 'id,name,description,isColumns,isCondition,isGroup,active,activityName,class,alias,columns,whereClause,groupByClause,havingClause,orderByClause,limitClause,ord', fields: 'id', orderBy: 'name asc'}")
-                
+        filtersVisualMapper = ReactiveVisualMapper.<Filter>createPushReactiveChain(this)
+                .always("{class: 'Filter', alias: 'fil', fields: 'id', where: '!isColumns', orderBy: 'name asc'}")
+                .setEntityColumns("[" +
+                        "{label: 'Name', expression: 'name'}," +
+                        "{label: 'Is Columns', expression: 'isColumns'}," +
+                        "{label: 'Is Condition', expression: 'isCondition'}," +
+                        "{label: 'Is Group', expression: 'isGroup'}," +
+                        "{label: 'Is Active', expression: 'active'}," +
+                        "{label: 'Activity Name', expression: 'activityName'}," +
+                        "{label: 'Class', expression: 'class'}," +
+                        "{label: 'Columns', expression: 'columns'}," +
+                        "{label: 'Where', expression: 'whereClause'}," +
+                        "{label: 'GroupBy', expression: 'groupByClause'}," +
+                        "{label: 'Having', expression: 'havingClause'}," +
+                        "{label: 'OrderBy', expression: 'orderByClause'}," +
+                        "{label: 'Limit', expression: 'limitClause'}" +
+                        "]")
                 .ifTrimNotEmpty(pm.searchTextProperty(), s -> where("lower(name) like ?", "%" + s.toLowerCase() + "%"))
-
+                //.ifFalse(??, ??)
                 .applyDomainModelRowStyle() // Colorizing the rows
                 .autoSelectSingleRow() // When the result is a singe row, automatically select it
                 .visualizeResultInto(pm.filtersVisualResultProperty())
+                // .setVisualSelectionProperty(pm.filtersVisualSelectionProperty())
+                // .setSelectedEntityHandler(entity -> graph.selectedMoneyAccount().set(entity))
+                .start();
+
+        fieldsVisualMapper = ReactiveVisualMapper.<Filter>createPushReactiveChain(this)
+                .always("{class: 'Filter', alias: 'fil', fields: 'id', where: 'isColumns', orderBy: 'name asc'}")
+                .setEntityColumns("[" +
+                        "{label: 'Name', expression: 'name'}," +
+                        "{label: 'Is Columns', expression: 'isColumns'}," +
+                        "{label: 'Is Condition', expression: 'isCondition'}," +
+                        "{label: 'Is Group', expression: 'isGroup'}," +
+                        "{label: 'Is Active', expression: 'active'}," +
+                        "{label: 'Activity Name', expression: 'activityName'}," +
+                        "{label: 'Class', expression: 'class'}," +
+                        "{label: 'Columns', expression: 'columns'}," +
+                        "{label: 'Where', expression: 'whereClause'}," +
+                        "{label: 'GroupBy', expression: 'groupByClause'}," +
+                        "{label: 'Having', expression: 'havingClause'}," +
+                        "{label: 'OrderBy', expression: 'orderByClause'}," +
+                        "{label: 'Limit', expression: 'limitClause'}" +
+                        "]")
+                //.ifTrimNotEmpty(pm.searchTextProperty(), s -> where("lower(name) like ?", "%" + s.toLowerCase() + "%"))
+                .applyDomainModelRowStyle() // Colorizing the rows
+                .autoSelectSingleRow() // When the result is a singe row, automatically select it
+                .visualizeResultInto(pm.fieldsVisualResultProperty())
                 // .setVisualSelectionProperty(pm.filtersVisualSelectionProperty())
                 // .setSelectedEntityHandler(entity -> graph.selectedMoneyAccount().set(entity))
                 .start();
@@ -157,10 +238,6 @@ final class FiltersActivity extends EventDependentViewDomainActivity implements 
     @Override
     protected void refreshDataOnActive() {
         filtersVisualMapper.refreshWhenActive();
-        /*
-        groupVisualMapper.refreshWhenActive();
-        masterVisualMapper.refreshWhenActive();
-        */
-        //moneyAccountVisualMapper.refreshWhenActive();
+        fieldsVisualMapper.refreshWhenActive();
     }
 }
