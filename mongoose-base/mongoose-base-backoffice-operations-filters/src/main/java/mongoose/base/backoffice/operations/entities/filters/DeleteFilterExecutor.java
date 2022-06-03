2c6d37dc0617b1111f59d52fb50e9ff3aefad847
goatsandtigers <danielnewman106@hotmail.com>
2022-06-03 11:02:46 +0100

    Added delete option to context menu.

---
+++ mongoose-base/mongoose-base-backoffice-operations-filters/src/main/java/mongoose/base/backoffice/operations/entities/filters/DeleteFilterExecutor.java
@@ -0,0 +1,42 @@
+package mongoose.base.backoffice.operations.entities.filters;
+
+import dev.webfx.framework.client.ui.controls.dialog.DialogContent;
+import dev.webfx.framework.client.ui.controls.dialog.DialogUtil;
+import dev.webfx.framework.shared.orm.entity.UpdateStore;
+import dev.webfx.platform.shared.async.Future;
+import dev.webfx.platform.shared.services.submit.SubmitArgument;
+import javafx.scene.layout.Pane;
+import mongoose.base.shared.entities.Filter;
+
+final class DeleteFilterExecutor {
+
+    static Future<Void> executeRequest(DeleteFilterRequest rq) {
+        return execute(rq.getFilter(), rq.getParentContainer());
+    }
+
+    private static Future<Void> execute(Filter filter, Pane parentContainer) {
+        if (filter == null) {
+            DialogContent dialogContent = new DialogContent().setContentText("No filter selected.");
+            DialogUtil.showModalNodeInGoldLayout(dialogContent, parentContainer);
+            DialogUtil.armDialogContentButtons(dialogContent, dialogCallback -> dialogCallback.closeDialog());
+        } else {
+            String msg = "Please confirm.\n\nDelete filter \"" + filter.getName() + "\"?";
+            DialogContent dialogContent = new DialogContent().setContentText(msg);
+            DialogUtil.showModalNodeInGoldLayout(dialogContent, parentContainer);
+            DialogUtil.armDialogContentButtons(dialogContent, dialogCallback -> {
+                deleteFilter(filter);
+                dialogCallback.closeDialog();
+            });
+        }
+        return Future.succeededFuture();
+    }
+
+    private static void deleteFilter(Filter filter) {
+        UpdateStore updateStore = UpdateStore.createAbove(filter.getStore());
+        updateStore.deleteEntity(filter);
+        updateStore.submitChanges(SubmitArgument.builder()
+                .setStatement("select set_transaction_parameters(false)")
+                .setDataSourceId(updateStore.getDataSourceId())
+                .build());
+    }
+}
