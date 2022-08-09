c3bc43a6cdedb12ed8d72a7ff5b095c51e6468bb
Bruno Salmon <dev.salmonb@gmail.com>
2022-08-09 17:42:41 +0100

    Moved platform modules from webfx-stack to webfx-platform (front-office)

--- modality-all/modality-all-frontoffice-application-gluon/src/main/java/module-info.java
+++ modality-all/modality-all-frontoffice-application-gluon/src/main/java/module-info.java
@@ -7,17 +7,18 @@ module modality.all.frontoffice.application.gluon {
     requires webfx.kit.openjfx;
     requires webfx.platform.boot.java;
     requires webfx.platform.console.java;
+    requires webfx.platform.json.java;
     requires webfx.platform.resource.java;
     requires webfx.platform.scheduler.java;
     requires webfx.platform.shutdown.java;
     requires webfx.platform.storage.java;
+    requires webfx.platform.windowhistory.java;
+    requires webfx.platform.windowlocation.java;
+    requires webfx.stack.com.websocket.bus.java;
     requires webfx.stack.com.websocket.java;
     requires webfx.stack.db.querysubmit.java.jdbc;
     requires webfx.stack.orm.dql.query.interceptor;
     requires webfx.stack.orm.dql.querypush.interceptor;
     requires webfx.stack.orm.dql.submit.interceptor;
-    requires webfx.stack.platform.json.java;
-    requires webfx.stack.platform.windowhistory.java;
-    requires webfx.stack.platform.windowlocation.java;
 
 }
