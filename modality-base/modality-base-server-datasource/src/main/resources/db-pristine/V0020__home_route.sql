44bbf92972a43d8d46852d5e4b445afd3ce74cea
Bruno Salmon <dev.salmonb@gmail.com>
2022-10-17 09:50:59 +0100

    Added route to home page operation in Flyway SQL file

---
+++ modality-base/modality-base-server-datasource/src/main/resources/db-pristine/V0020__home_route.sql
@@ -0,0 +1,1 @@
+INSERT INTO public.operation (operation_code, i18n_code, name, label_id, backend, frontend, public, read_only) VALUES ('RouteToHome', 'Home', 'Home areas', NULL, true, false, true, false);
