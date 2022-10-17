e365dbd91c794afaba69d583a3a0b396e119ec4b
Bruno Salmon <dev.salmonb@gmail.com>
2022-10-17 09:51:16 +0100

    Added route to kitchen page operation in Flyway SQL file

---
+++ modality-base/modality-base-server-datasource/src/main/resources/db-pristine/V0021__kitchen_route.sql
@@ -0,0 +1,1 @@
+INSERT INTO public.operation (operation_code, i18n_code, name, label_id, backend, frontend, public, read_only) VALUES ('RouteToKitchen', 'Kitchen', 'Kitchen dashboard', NULL, true, false, true, false);
