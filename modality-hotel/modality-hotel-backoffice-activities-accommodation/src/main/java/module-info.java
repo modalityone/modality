0c522b78c5c300b67564459c514c6bcc36b2bda0
Bruno Salmon <dev.salmonb@gmail.com>
2023-04-21 15:58:12 +0100

    Added modality-hotel-backoffice-activities-accommodation (empty)

---
+++ modality-hotel/modality-hotel-backoffice-activities-accommodation/src/main/java/module-info.java
@@ -0,0 +1,26 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module modality.hotel.backoffice.activities.accommodation {
+
+    // Direct dependencies modules
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires modality.base.client.activity;
+    requires webfx.platform.windowhistory;
+    requires webfx.stack.orm.domainmodel.activity;
+    requires webfx.stack.routing.router;
+    requires webfx.stack.routing.router.client;
+    requires webfx.stack.routing.uirouter;
+    requires webfx.stack.ui.operation;
+    requires webfx.stack.ui.operation.action;
+
+    // Exported packages
+    exports one.modality.hotel.backoffice.activities.accommodation;
+    exports one.modality.hotel.backoffice.activities.accommodation.routing;
+    exports one.modality.hotel.backoffice.operations.routes.accommodation;
+
+    // Provided services
+    provides dev.webfx.stack.routing.uirouter.UiRoute with one.modality.hotel.backoffice.activities.accommodation.AccommodationUiRoute;
+    provides dev.webfx.stack.routing.uirouter.operations.RouteRequestEmitter with one.modality.hotel.backoffice.activities.accommodation.RouteToAccommodationRequestEmitter;
+
+}
---
+++ modality-hotel/modality-hotel-backoffice-activities-accommodation/src/main/java/one/modality/hotel/backoffice/activities/accommodation/AccommodationUiRoute.java
@@ -0,0 +1,25 @@
+package one.modality.hotel.backoffice.activities.accommodation;
+
+import one.modality.hotel.backoffice.activities.accommodation.routing.AccommodationRouting;
+import dev.webfx.stack.orm.domainmodel.activity.viewdomain.impl.ViewDomainActivityContextFinal;
+import dev.webfx.stack.routing.uirouter.UiRoute;
+import dev.webfx.stack.routing.uirouter.impl.UiRouteImpl;
+import dev.webfx.stack.routing.router.util.PathBuilder;
+
+/**
+ * @author Bruno Salmon
+ */
+public final class AccommodationUiRoute extends UiRouteImpl {
+
+    public AccommodationUiRoute() {
+        super(uiRoute());
+    }
+
+    public static UiRoute<?> uiRoute() {
+        return UiRoute.createRegex(PathBuilder.toRegexPath(AccommodationRouting.getAnyPath())
+                , false
+                , AccommodationActivity::new
+                , ViewDomainActivityContextFinal::new
+        );
+    }
+}
