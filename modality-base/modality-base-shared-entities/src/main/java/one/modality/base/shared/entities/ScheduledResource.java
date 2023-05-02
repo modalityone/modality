e186b2e6404fa9428952e8f7a386b844599b5756
Bruno Salmon <dev.salmonb@gmail.com>
2023-05-02 13:52:56 +0100

    Added ResourceConfiguration entity

--- modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/ScheduledResource.java
+++ modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/ScheduledResource.java
@@ -1,6 +1,7 @@
 package one.modality.base.shared.entities;
 
 import dev.webfx.stack.orm.entity.Entity;
+import dev.webfx.stack.orm.entity.EntityId;
 import one.modality.base.shared.entities.markers.EntityHasDate;
 
 /**
@@ -8,5 +9,42 @@ import one.modality.base.shared.entities.markers.EntityHasDate;
  */
 public interface ScheduledResource extends Entity,
         EntityHasDate {
+
+    default void setResourceConfiguration(Object event) {
+        setForeignField("configuration", event);
+    }
+
+    default EntityId getResourceConfigurationId() {
+        return getForeignEntityId("configuration");
+    }
+
+    default ResourceConfiguration getResourceConfiguration() {
+        return getForeignEntity("configuration");
+    }
+
+    default void setAvailable(Boolean online) {
+        setFieldValue("available", online);
+    }
+
+    default Boolean isAvailable() {
+        return getBooleanFieldValue("available");
+    }
+
+    default void setOnline(Boolean online) {
+        setFieldValue("online", online);
+    }
+
+    default Boolean isOnline() {
+        return getBooleanFieldValue("online");
+    }
+
+    default void setMax(Integer max) {
+        setFieldValue("max", max);
+    }
+
+    default Integer getMax() {
+        return getIntegerFieldValue("max");
+    }
+
 
 }
---
+++ modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/impl/ResourceConfigurationImpl.java
@@ -0,0 +1,23 @@
+package one.modality.base.shared.entities.impl;
+
+import dev.webfx.stack.orm.entity.EntityId;
+import dev.webfx.stack.orm.entity.EntityStore;
+import dev.webfx.stack.orm.entity.impl.DynamicEntity;
+import dev.webfx.stack.orm.entity.impl.EntityFactoryProviderImpl;
+import one.modality.base.shared.entities.ResourceConfiguration;
+
+/**
+ * @author Bruno Salmon
+ */
+public final class ResourceConfigurationImpl extends DynamicEntity implements ResourceConfiguration {
+
+    public ResourceConfigurationImpl(EntityId id, EntityStore store) {
+        super(id, store);
+    }
+
+    public static final class ProvidedFactory extends EntityFactoryProviderImpl<ResourceConfiguration> {
+        public ProvidedFactory() {
+            super(ResourceConfiguration.class, ResourceConfigurationImpl::new);
+        }
+    }
+}
