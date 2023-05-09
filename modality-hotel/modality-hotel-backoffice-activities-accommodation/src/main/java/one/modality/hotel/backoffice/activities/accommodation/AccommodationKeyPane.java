72a428dc5ebc44ec3fcf53ae3a1bf59132a1991c
dan <danielnewman106@hotmail.com>
2023-05-09 14:14:08 +0100

    Added coloured rectangles to show event colours.

--- modality-hotel/modality-hotel-backoffice-activities-accommodation/src/main/java/one/modality/hotel/backoffice/activities/accommodation/AccommodationKeyPane.java
+++ modality-hotel/modality-hotel-backoffice-activities-accommodation/src/main/java/one/modality/hotel/backoffice/activities/accommodation/AccommodationKeyPane.java
@@ -1,8 +1,12 @@
 package one.modality.hotel.backoffice.activities.accommodation;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import one.modality.base.shared.entities.Event;
 
 import java.util.ArrayList;
@@ -11,16 +15,22 @@ import java.util.List;
 
 public class AccommodationKeyPane extends VBox {
 
+    private static final double COLOR_BLOCK_HEIGHT = 20;
     private List<Event> events = Collections.emptyList();
 
     public void setEvents(List<Event> events) {
         this.events = new ArrayList<>(events);
-        getChildren().clear();
+        List<HBox> rows = new ArrayList<>(events.size());
         for (Event event : events) {
+            Rectangle rectangle = new Rectangle(COLOR_BLOCK_HEIGHT, COLOR_BLOCK_HEIGHT);
+            rectangle.setFill(getEventColor(event));
             Label label = new Label(event.getName());
-            label.setTextFill(getEventColor(event));
-            getChildren().add(label);
+            HBox row = new HBox(rectangle, label);
+            row.setPadding(new Insets(8, 0, 0, 0));
+            row.setAlignment(Pos.CENTER_LEFT);
+            rows.add(row);
         }
+        getChildren().setAll(rows);
     }
 
     public Color getEventColor(Event event) {
