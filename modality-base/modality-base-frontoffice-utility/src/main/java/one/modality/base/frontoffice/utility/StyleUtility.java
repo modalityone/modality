ff57c3a2185d7797d043471091fb0d0a7884d84e
tylertuan37 <tylernkt.working@gmail.com>
2023-06-23 16:55:38 +0100

    More reformat

--- modality-base/modality-base-frontoffice-utility/src/main/java/one/modality/base/frontoffice/utility/StyleUtility.java
+++ modality-base/modality-base-frontoffice-utility/src/main/java/one/modality/base/frontoffice/utility/StyleUtility.java
@@ -4,6 +4,7 @@ public class StyleUtility {
     public static String MAIN_BLUE = "#0096D6";
     public static String POSITIVE_GREEN = "#41BA4D";
     public static String INPUT_BORDER = "#838788";
+    public static String SEPARATOR_GRAY = "#D9D9D9";
     public static int MAIN_TEXT_SIZE = 12;
     public static int SUB_TEXT_SIZE = 9;
 }
--- modality-event/modality-event-frontoffice-activities-account/src/main/java/one/modality/event/frontoffice/activities/account/AccountFriendsAndFamilyActivity.java
+++ modality-event/modality-event-frontoffice-activities-account/src/main/java/one/modality/event/frontoffice/activities/account/AccountFriendsAndFamilyActivity.java
@@ -9,23 +9,26 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import one.modality.base.frontoffice.states.PersonPM;
 import one.modality.base.frontoffice.utility.GeneralUtility;
+import one.modality.base.frontoffice.utility.StyleUtility;
 import one.modality.base.frontoffice.utility.TextUtility;
 
 import java.util.List;
 
 public class AccountFriendsAndFamilyActivity extends ViewDomainActivityBase implements ButtonFactoryMixin {
-    private void rebuildMembersList(ObservableList<PersonPM> persons, VBox membersContainer, VBox membersList, VBox memberInformation, VBox container) {
+    private void rebuildMembersList(ObservableList<PersonPM> persons, BorderPane membersContainer, VBox membersList, VBox memberInformation, BorderPane container) {
         membersList.getChildren().removeAll(membersList.getChildren());
 
         persons.forEach(personPM -> {
-            Button b = new Button();
-
             BorderPane bpL = new BorderPane();
             BorderPane bpR = new BorderPane();
+            Separator s = new Separator();
+
+            s.setBorder(new Border(new BorderStroke(Color.web(StyleUtility.SEPARATOR_GRAY), BorderStrokeStyle.NONE, null, BorderStroke.THIN)));
 
             bpL.setLeft(GeneralUtility.createRadioCheckBoxBySelection(FXAccount.toBeDeletedPerson, personPM.NAME_FULL.get()));
             bpL.setCenter(bpR);
@@ -34,11 +37,12 @@ public class AccountFriendsAndFamilyActivity extends ViewDomainActivityBase impl
             bpR.setCenter(
                     GeneralUtility.createSplitRow(TextUtility.getBindedText(personPM.NAME_FULL, TextUtility::getMainText), TextUtility.getSubText("Kinship"), 75, 0)
             );
+            bpR.setBottom(s);
 
             bpR.setOnMouseClicked(e -> {
                 rebuildMemberInformation(memberInformation);
                 container.getChildren().remove(membersContainer);
-                container.getChildren().add(memberInformation);
+                container.setCenter(memberInformation);
                 FXAccount.viewedPersonPM.set(personPM.PERSON);
                 FXAccount.viewedPersonPM.setASSOCIATE_PM(personPM);
             });
@@ -61,16 +65,17 @@ public class AccountFriendsAndFamilyActivity extends ViewDomainActivityBase impl
                 50, 10
         ));
 
-        memberInformation.getChildren().addAll(
-                nameContainer,
-                AccountUtility.displayInformation(this, this, FXAccount.viewedPersonPM)
-        );
+        memberInformation.getChildren().add(LayoutUtil.createVerticalScrollPane(
+                (Region) GeneralUtility.createVList(0, 0, nameContainer, AccountUtility.displayInformation(this, this, FXAccount.viewedPersonPM))
+        ));
+
+
     }
 
     @Override
     public Node buildUi() {
-        VBox container = new VBox();
-        VBox membersContainer = new VBox();
+        BorderPane container = new BorderPane();
+        BorderPane membersContainer = new BorderPane();
         VBox membersList = new VBox();
         VBox memberInformation = new VBox();
         Button friendsFamilyHome = new Button("Friends and Family");
@@ -85,29 +90,27 @@ public class AccountFriendsAndFamilyActivity extends ViewDomainActivityBase impl
         Button addMember = new Button("Add member");
         Button deleteMember = new Button("Delete selected member");
 
-        membersContainer.getChildren().addAll(
-                membersList,
-                GeneralUtility.createHList(10, 0, addMember, deleteMember)
-        );
+        membersContainer.setCenter(LayoutUtil.createVerticalScrollPane(membersList));
+        membersContainer.setBottom(GeneralUtility.createHList(10, 0, addMember, deleteMember));
 
         addMember.setOnAction(e -> {
             FXAccount.viewedPersonPM.setNew();
             rebuildMemberInformation(memberInformation);
             container.getChildren().remove(membersContainer);
-            container.getChildren().add(memberInformation);
+            container.setCenter(memberInformation);
         });
 
         friendsFamilyHome.setOnAction(e -> {
             container.getChildren().remove(memberInformation);
-            container.getChildren().add(membersContainer);
+            container.setCenter(membersContainer);
         });
 
-        container.getChildren().addAll(
-                friendsFamilyHome,
-                membersContainer
-        );
+        container.setTop(friendsFamilyHome);
+        container.setCenter(membersContainer);
 
-        return LayoutUtil.createVerticalScrollPane(container);
+        container.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+
+        return container;
     }
 
 }
