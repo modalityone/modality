e73308e2ab5136c601d3c83c141f10ce649db94e
bjvickers <benjamin.vickers@yahoo.co.uk>
2023-08-14 10:04:52 +0100

    updated organizatoin entity to retrieve associated kdm id

--- modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/Organization.java
+++ modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/Organization.java
@@ -1,5 +1,6 @@
 package one.modality.base.shared.entities;
 
+import dev.webfx.stack.orm.entity.EntityId;
 import one.modality.base.shared.entities.markers.EntityHasIcon;
 import one.modality.base.shared.entities.markers.EntityHasLabel;
 import one.modality.base.shared.entities.markers.EntityHasName;
@@ -11,4 +12,16 @@ public interface Organization extends
         EntityHasName,
         EntityHasLabel,
         EntityHasIcon {
+
+    default void setKdmCenter(KdmCenter kdmCenter) {
+        setForeignField("kdmCenter", kdmCenter);
+    }
+
+    default EntityId getKdmCenterId() {
+        return getForeignEntityId("kdmCenter");
+    }
+
+    default KdmCenter getKdmCenter() {
+        return getForeignEntity("kdmCenter");
+    }
 }
