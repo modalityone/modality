f30a7342768d13e5199d6ee9c410beb2dca63570
bjvickers <benjamin.vickers@yahoo.co.uk>
2023-08-17 16:12:42 +0100

    latest

--- modality-base/modality-base-server-geoimport/src/main/java/module-info.java
+++ modality-base/modality-base-server-geoimport/src/main/java/module-info.java
@@ -3,16 +3,11 @@
 module modality.base.server.geoimport {
 
     // Direct dependencies modules
-    requires java.base;
     requires modality.base.shared.entities;
-    requires webfx.extras.webtext.util;
-    requires webfx.platform.async;
     requires webfx.platform.boot;
     requires webfx.platform.console;
     requires webfx.platform.fetch;
     requires webfx.platform.json;
-    requires webfx.platform.scheduler;
-    requires webfx.platform.util;
     requires webfx.stack.orm.datasourcemodel.service;
     requires webfx.stack.orm.domainmodel;
     requires webfx.stack.orm.entity;
