b715df3a556f2a5b1c7116490f121ec234626479
Bruno Salmon <dev.salmonb@gmail.com>
2023-11-05 13:14:10 +0000

    Added SSO => ModalityUserPrincipal provisioning code in FXModalityUserPrincipal

--- modality-crm/modality-crm-client-authn-fx/src/main/java/one/modality/crm/shared/services/authn/fx/FXModalityUserPrincipal.java
+++ modality-crm/modality-crm-client-authn-fx/src/main/java/one/modality/crm/shared/services/authn/fx/FXModalityUserPrincipal.java
@@ -2,9 +2,14 @@ package one.modality.crm.shared.services.authn.fx;
 
 import dev.webfx.kit.util.properties.FXProperties;
 import dev.webfx.platform.console.Console;
+import dev.webfx.stack.authn.UserClaims;
+import dev.webfx.stack.orm.datasourcemodel.service.DataSourceModelService;
+import dev.webfx.stack.orm.entity.EntityStore;
 import dev.webfx.stack.session.state.client.fx.FXLoggedIn;
+import dev.webfx.stack.session.state.client.fx.FXUserClaims;
 import dev.webfx.stack.session.state.client.fx.FXUserId;
 import javafx.beans.property.*;
+import one.modality.base.shared.entities.Person;
 import one.modality.crm.shared.services.authn.ModalityUserPrincipal;
 
 /**
@@ -41,9 +46,34 @@ public final class FXModalityUserPrincipal {
     static {
         FXProperties.runNowAndOnPropertiesChange(() -> {
             Object userId = FXUserId.getUserId();
-            setModalityUserPrincipal(userId instanceof ModalityUserPrincipal ? (ModalityUserPrincipal) userId : null);
+            if (userId instanceof ModalityUserPrincipal) // Happens when directly logging using Modality username/password
+                setModalityUserPrincipal((ModalityUserPrincipal) userId);
+            else { // User provisioning code: SSO login => ModalityUserPrincipal TODO: Move this provisioning code on server
+                // Checking UserClaims (when logging through SSO)
+                UserClaims userClaims = FXUserClaims.getUserClaims();
+                if (userClaims != null) { // Yes, the user logged-in through SSO
+                    String email = userClaims.getEmail();
+                    if (email != null) { // Yes, the user provided an email
+                        // Loading the Modality user (i.e. Person) owning a frontend account whose username is that exact same email
+                        EntityStore.create(DataSourceModelService.getDefaultDataSourceModel())
+                                .<Person>executeQuery("select id, frontendAccount.id from Person where frontendAccount.(username=? and corporation=1) order by id limit 1", email)
+                                .onFailure(Console::log)
+                                .onSuccess(personList -> {
+                                    // We should get only one matching person
+                                    if (personList.size() == 1) {
+                                        // Getting the Person entity
+                                        Person person = personList.get(0);
+                                        // Creating the ModalityUserPrincipal instance from Person entity
+                                        ModalityUserPrincipal modalityUserPrincipal = new ModalityUserPrincipal(person.getPrimaryKey(), person.getForeignEntityId("frontendAccount").getPrimaryKey());
+                                        // Setting FXModalityUserPrincipal with this instance
+                                        setModalityUserPrincipal(modalityUserPrincipal);
+                                    }
+                                });
+                    }
+                }
+            }
             loggedInProperty.set(getModalityUserPrincipal() != null && FXLoggedIn.isLoggedIn());
-        }, FXUserId.userIdProperty(), FXLoggedIn.loggedInProperty());
+        }, FXUserId.userIdProperty(), FXUserClaims.userClaimsProperty(), FXLoggedIn.loggedInProperty());
     }
 
 }
