22be63551951d4b1f2a7007af61ed6bce1b3dc6c
Bruno Salmon <dev.salmonb@gmail.com>
2023-11-05 13:14:10 +0000

    Added SSO => ModalityUserPrincipal provisioning code in FXModalityUserPrincipal

--- modality-crm/modality-crm-client-authn-fx/src/main/java/module-info.java
+++ modality-crm/modality-crm-client-authn-fx/src/main/java/module-info.java
@@ -11,6 +11,7 @@ module modality.crm.client.authn.fx {
     requires transitive modality.crm.shared.authn;
     requires webfx.kit.util;
     requires webfx.platform.console;
+    requires webfx.stack.authn;
     requires webfx.stack.orm.datasourcemodel.service;
     requires webfx.stack.orm.domainmodel;
     requires webfx.stack.orm.entity;
