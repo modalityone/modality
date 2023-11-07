e7d9b920d682210c4256b93a76f6d0abaad20277
Bruno Salmon <dev.salmonb@gmail.com>
2023-11-07 10:48:44 +0000

    Removed unnecessary pref width computation of front-office home buttons

--- modality-base/modality-base-frontoffice-application/src/main/java/module-info.java
+++ modality-base/modality-base-frontoffice-application/src/main/java/module-info.java
@@ -3,7 +3,6 @@
 module modality.base.frontoffice.application {
 
     // Direct dependencies modules
-    requires java.base;
     requires javafx.base;
     requires javafx.controls;
     requires javafx.graphics;
