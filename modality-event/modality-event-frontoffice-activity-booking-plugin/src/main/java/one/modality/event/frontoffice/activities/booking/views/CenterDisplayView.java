948ebb6233a88fa8cc33d2317e62ba9deabd23b7
Bruno Salmon <dev.salmonb@gmail.com>
2023-11-15 16:59:28 +0000

    Added local centre button selector (front-office booking page)

--- modality-event/modality-event-frontoffice-activity-booking-plugin/src/main/java/one/modality/event/frontoffice/activities/booking/views/CenterDisplayView.java
+++ modality-event/modality-event-frontoffice-activity-booking-plugin/src/main/java/one/modality/event/frontoffice/activities/booking/views/CenterDisplayView.java
@@ -1,6 +1,9 @@
 package one.modality.event.frontoffice.activities.booking.views;
 
 import dev.webfx.extras.imagestore.ImageStore;
+import dev.webfx.extras.panes.ScaleMode;
+import dev.webfx.extras.panes.ScalePane;
+import dev.webfx.kit.launcher.WebFxKitLauncher;
 import dev.webfx.kit.util.properties.FXProperties;
 import dev.webfx.platform.conf.SourcesConfig;
 import dev.webfx.stack.i18n.I18n;
@@ -10,33 +13,36 @@ import dev.webfx.stack.ui.controls.button.ButtonFactoryMixin;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import one.modality.base.client.mainframe.dialogarea.fx.FXMainFrameDialogArea;
-import one.modality.base.frontoffice.fx.FXAccount;
 import one.modality.base.frontoffice.fx.FXBooking;
-import one.modality.base.frontoffice.states.BookingPM;
 import one.modality.base.frontoffice.utility.GeneralUtility;
 import one.modality.base.frontoffice.utility.StyleUtility;
 import one.modality.base.frontoffice.utility.TextUtility;
 import one.modality.base.shared.entities.Organization;
+import one.modality.crm.backoffice.organization.fx.FXOrganization;
 
 public final class CenterDisplayView {
 
     public Node getView(ButtonFactoryMixin factoryMixin, ViewDomainActivityBase activityBase) {
 
+        String organizationJson = "{class: 'Organization', alias: 'o', where: '!closed and name!=`ISC` and exists(select Event where organization=o and endDate > now())', orderBy: 'country.name,name'}";
+        if (WebFxKitLauncher.supportsSvgImageFormat())
+            organizationJson = "{class: 'Organization', alias: 'o', where: '!closed and name!=`ISC` and exists(select Event where organization=o and endDate > now())', orderBy: 'country.name,name', columns: [{expression: '[image(`images/s16/organizations/svg/` + (type=2 ? `kmc` : type=3 ? `kbc` : type=4 ? `branch` : `generic`) + `.svg`),name]'}] }";
         EntityButtonSelector<Organization> centersButtonSelector = new EntityButtonSelector<>(
-                "{class: 'Organization', orderBy: 'name'}",
-                factoryMixin, FXMainFrameDialogArea::getDialogArea, activityBase.getDataSourceModel()
+                organizationJson, factoryMixin, FXMainFrameDialogArea::getDialogArea, activityBase.getDataSourceModel()
         );
 
-        centersButtonSelector.selectedItemProperty().bindBidirectional(FXAccount.ownerPM.LOCAL_CENTER);
+        centersButtonSelector.selectedItemProperty().bindBidirectional(FXOrganization.organizationProperty());
 
         String staticMapUrl = SourcesConfig.getSourcesRootConfig().childConfigAt("modality.event.frontoffice.activity.booking").getString("centreStaticMapUrl");
         ImageView map = ImageStore.createImageView(staticMapUrl);
@@ -56,25 +62,25 @@ public final class CenterDisplayView {
                 ),50, 10
         );
 
-        Text changeLocation = TextUtility.getText(BookingPM.CHANGE_CENTER.get() ? "Confirm change" : "Change your location", 10, StyleUtility.RUPAVAJRA_WHITE);
+        Text changeLocation = TextUtility.getText("Change your location", 10, StyleUtility.RUPAVAJRA_WHITE);
         changeLocation.setOnMouseClicked(event -> {
-            if (BookingPM.CHANGE_CENTER.get()) FXAccount.updatePerson(FXAccount.ownerPM);
-            BookingPM.CHANGE_CENTER.set(!BookingPM.CHANGE_CENTER.get());
         });
         changeLocation.setTextAlignment(TextAlignment.CENTER);
 
         Label localCenterNameLabel = GeneralUtility.createLabel("localCentreName"/* FXAccount.ownerPM.LOCAL_CENTER.getValue().getName()*/, Color.web(StyleUtility.VICTOR_BATTLE_BLACK), StyleUtility.MAIN_TEXT_SIZE);
         localCenterNameLabel.setTextAlignment(TextAlignment.CENTER);
 
-        VBox container = new VBox(
+        Button button = centersButtonSelector.getButton();
+        button.setMaxWidth(Region.USE_PREF_SIZE);
+        ScalePane scalePane = new ScalePane(ScaleMode.FIT_WIDTH, button);
+        scalePane.setMaxScale(2.5);
+        VBox container = new VBox(20,
                 //TextUtility.getSubText("Your Country", StyleUtility.RUPAVAJRA_WHITE),
                 //BookingPM.CHANGE_CENTER.get() ? countriesButtonSelector.getButton() : GeneralUtility.createLabel(FXAccount.ownerPM.ADDRESS_COUNTRY.getValue().getName(), Color.web(StyleUtility.VICTOR_BATTLE_BLACK), StyleUtility.MAIN_TEXT_SIZE),
-                GeneralUtility.createSpace(20),
                 I18n.bindI18nProperties(TextUtility.getSubText(null, StyleUtility.RUPAVAJRA_WHITE), "yourLocalCentre"),
-                BookingPM.CHANGE_CENTER.get() ? centersButtonSelector.getButton() : localCenterNameLabel,
-                GeneralUtility.createSpace(20),
+                scalePane,
+                GeneralUtility.createSpace(10),
                 location,
-                GeneralUtility.createSpace(20),
                 changeLocation
         );
 
