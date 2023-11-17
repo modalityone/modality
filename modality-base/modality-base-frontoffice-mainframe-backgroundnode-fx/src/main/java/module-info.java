00ca95ca30ada2ff973a3b67b50a644f9b74fc91
Bruno Salmon <dev.salmonb@gmail.com>
2023-11-17 15:51:28 +0000

    Introduced possible background node (ex: WebView) in front-office (2)

---
+++ modality-base/modality-base-frontoffice-mainframe-backgroundnode-fx/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module modality.base.frontoffice.mainframe.backgroundnode.fx {
+
+    // Direct dependencies modules
+    requires javafx.base;
+    requires javafx.graphics;
+
+    // Exported packages
+    exports one.modality.base.frontoffice.mainframe.backgroundnode.fx;
+
+}
