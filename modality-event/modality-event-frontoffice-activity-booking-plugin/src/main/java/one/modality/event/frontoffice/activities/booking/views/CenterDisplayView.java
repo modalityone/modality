4a8e8f51e780f17e853f90a45b7a896a5dfe4ca9
Bruno Salmon <dev.salmonb@gmail.com>
2023-11-23 15:33:18 +0000

    Extracted MapView from CenterDisplayView

--- modality-event/modality-event-frontoffice-activity-booking-plugin/src/main/java/one/modality/event/frontoffice/activities/booking/views/CenterDisplayView.java
+++ modality-event/modality-event-frontoffice-activity-booking-plugin/src/main/java/one/modality/event/frontoffice/activities/booking/views/CenterDisplayView.java
@@ -5,31 +5,21 @@ import dev.webfx.extras.panes.ScaleMode;
 import dev.webfx.extras.panes.ScalePane;
 import dev.webfx.kit.launcher.WebFxKitLauncher;
 import dev.webfx.kit.util.properties.FXProperties;
-import dev.webfx.platform.conf.Config;
-import dev.webfx.platform.conf.SourcesConfig;
 import dev.webfx.stack.i18n.I18n;
 import dev.webfx.stack.orm.domainmodel.activity.viewdomain.impl.ViewDomainActivityBase;
 import dev.webfx.stack.orm.entity.controls.entity.selector.EntityButtonSelector;
 import dev.webfx.stack.ui.controls.button.ButtonFactoryMixin;
 import javafx.application.Platform;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.SVGPath;
-import javafx.scene.shape.StrokeLineCap;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import one.modality.base.client.mainframe.dialogarea.fx.FXMainFrameDialogArea;
@@ -40,15 +30,6 @@ import one.modality.base.shared.entities.Organization;
 import one.modality.crm.backoffice.organization.fx.FXOrganization;
 
 public final class CenterDisplayView {
-
-    private static final String MARKER_SVG_PATH = "m 8.0408158,27.932173 c 0.2040781,-3.908096 2.3550612,-10.256967 5.4754162,-14.250776 1.699971,-2.177513 2.48363,-4.2978848 2.48363,-5.5856178 A 8.0488411,8.0488411 0 0 0 8.0000002,2.1599999e-7 v 0 A 8.0488411,8.0488411 0 0 0 1.378808e-4,8.0957792 c 0,1.287733 0.7816191992,3.4081048 2.4836307192,5.5856178 3.1203545,3.99585 5.2754194,10.34268 5.475416,14.250776 z";
-    private static final String ZOOM_PLUS_SVG_PATH = "M 13,8 H 3 M 8,13 V 3";
-    private static final String ZOOM_MINUS_SVG_PATH = "M 13,8 H 3";
-    private static final int ZOOM_MIN = 5, ZOOM_MAX = 18, ZOOM_INITIAL = 12;
-
-    private final IntegerProperty zoomProperty = new SimpleIntegerProperty(ZOOM_INITIAL);
-    private final Button zoomInButton = createZoomButton(ZOOM_PLUS_SVG_PATH);
-    private final Button zoomOutButton = createZoomButton(ZOOM_MINUS_SVG_PATH);
 
     public Node getView(ButtonFactoryMixin factoryMixin, ViewDomainActivityBase activityBase) {
 
@@ -61,28 +42,20 @@ public final class CenterDisplayView {
 
         centersButtonSelector.selectedItemProperty().bindBidirectional(FXOrganization.organizationProperty());
 
-        ImageView centreStaticMapImageView = new ImageView();
-        centreStaticMapImageView.setPreserveRatio(true);
 
         Hyperlink centreWebsiteLink = GeneralUtility.setupLabeled(new Hyperlink(), "localCentreWebsite", Color.WHITE, StyleUtility.MAIN_TEXT_SIZE);
         Hyperlink centreAddressLink = GeneralUtility.setupLabeled(new Hyperlink(),"localCentreAddress", Color.WHITE, StyleUtility.MAIN_TEXT_SIZE);
         Hyperlink centrePhoneLink = GeneralUtility.setupLabeled(new Hyperlink(), "localCentrePhone", Color.WHITE, StyleUtility.MAIN_TEXT_SIZE);
         Hyperlink centreEmailLabel = GeneralUtility.setupLabeled(new Hyperlink(), "localCentreEmail", Color.WHITE, StyleUtility.MAIN_TEXT_SIZE);
 
+        MapView organizationMapView = new MapView();
+        organizationMapView.entityProperty().bind(FXOrganization.organizationProperty());
+
         FXProperties.runNowAndOnPropertiesChange(e -> {
             Organization organization = FXOrganization.getOrganization();
             if (organization != null) {
                 organization.onExpressionLoaded("domainName,latitude,longitude,street,cityName,postCode,country.name,phone,email")
                     .onSuccess(ignored -> Platform.runLater(() -> {
-                        Config webConfig = SourcesConfig.getSourcesRootConfig().childConfigAt("webfx.stack.com.client.websocket");
-                        String serverHost = webConfig.getString("serverHost");
-                        boolean serverSSL = webConfig.getBoolean("serverSSL");
-                        String mapUrl = (serverSSL ? "https://" : "http://") + serverHost + "/map/organization/" + organization.getPrimaryKey() + "?zoom=" + zoomProperty.get();
-                        Image image = new Image(mapUrl, true);
-                        image.progressProperty().addListener(observable -> {
-                            if (image.getProgress() >= 1)
-                                centreStaticMapImageView.setImage(image);
-                        });
                         String domainName = organization.getStringFieldValue("domainName");
                         FXProperties.setEvenIfBound(centreWebsiteLink.textProperty(), domainName);
                         centreWebsiteLink.setOnAction(e2 -> WebFxKitLauncher.getApplication().getHostServices().showDocument("https://" + domainName));
@@ -96,7 +69,7 @@ public final class CenterDisplayView {
                         centreEmailLabel.setOnAction(e2 -> WebFxKitLauncher.getApplication().getHostServices().showDocument("mailto://" + email));
                     }));
             }
-        }, FXOrganization.organizationProperty(), zoomProperty);
+        }, FXOrganization.organizationProperty());
 
         VBox centreInfoBox = GeneralUtility.createVList(10, 0,
                 centreWebsiteLink,
@@ -107,26 +80,8 @@ public final class CenterDisplayView {
         centreInfoBox.setAlignment(Pos.CENTER_LEFT);
         centreInfoBox.setPadding(new Insets(0, 0, 0, 35));
 
-        zoomInButton.setOnAction(e -> incrementZoom( +1));
-        zoomOutButton.setOnAction(e -> incrementZoom(-1));
-        VBox zoomBar = new VBox(5, zoomInButton, zoomOutButton);
-        zoomBar.setAlignment(Pos.BOTTOM_RIGHT);
-        zoomBar.setPadding(new Insets(5));
-
-        SVGPath markerSvgPath = new SVGPath();
-        markerSvgPath.setContent(MARKER_SVG_PATH);
-        markerSvgPath.setFill(Color.web("#EA4335"));
-        markerSvgPath.setStroke(Color.web("#DA352D"));
-        // We want the bottom tip of the marker to be in the center of the map. The SVG is 16px high, so we need to move
-        // it up by 8px (otherwise this will be the point at y = 8px in SVG that will be in the center).
-        markerSvgPath.setTranslateY(-8);
-
-        // We add a little white circle on top of the red marker
-        Circle markerCircle = new Circle(3.5, Color.WHITE);
-        markerCircle.setTranslateY(-14); // Moving it up to the right position
-
         Node location = new ColumnsPane(
-                new StackPane(new ScalePane(ScaleMode.FIT_WIDTH, centreStaticMapImageView), zoomBar, markerSvgPath, markerCircle),
+                organizationMapView.buildMapNode(),
                 centreInfoBox
         );
 
@@ -155,27 +110,6 @@ public final class CenterDisplayView {
         container.setPadding(new Insets(35));
         container.setAlignment(Pos.CENTER);
 
-        FXProperties.runNowAndOnPropertiesChange(() -> centreStaticMapImageView.setFitWidth(container.getWidth() * 0.25), container.widthProperty());
-
         return container;
-    }
-
-    private void incrementZoom(int deltaZoom) {
-        int zoomValue = zoomProperty.get() + deltaZoom;
-        zoomValue = Math.max(ZOOM_MIN, Math.min(ZOOM_MAX, zoomValue));
-        zoomProperty.set(zoomValue);
-        zoomInButton.setDisable(zoomValue == ZOOM_MAX);
-        zoomOutButton.setDisable(zoomValue == ZOOM_MIN);
-    }
-
-    private static Button createZoomButton(String path) {
-        Button b = new Button();
-        SVGPath svgPath = new SVGPath();
-        svgPath.setContent(path);
-        svgPath.setStroke(Color.BLACK);
-        svgPath.setStrokeLineCap(StrokeLineCap.ROUND);
-        b.setGraphic(svgPath);
-        b.setMinSize(24, 24); // For the web version, otherwise the minus button is not square (because minus SVG is not square)
-        return b;
     }
 }
