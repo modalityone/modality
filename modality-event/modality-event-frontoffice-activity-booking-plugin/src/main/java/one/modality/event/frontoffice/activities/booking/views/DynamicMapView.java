b3464af3e3aa6e77e8ef44a213e9276575e3f06b
Bruno Salmon <dev.salmonb@gmail.com>
2023-12-04 13:11:27 +0000

    Fixed: google markers were not recreated when navigating back in the browser

--- modality-event/modality-event-frontoffice-activity-booking-plugin/src/main/java/one/modality/event/frontoffice/activities/booking/views/DynamicMapView.java
+++ modality-event/modality-event-frontoffice-activity-booking-plugin/src/main/java/one/modality/event/frontoffice/activities/booking/views/DynamicMapView.java
@@ -31,10 +31,11 @@ public class DynamicMapView extends MapViewBase {
         webEngine.setOnError(event -> Console.log("Received error event: " + event));
         FXProperties.runNowAndOnPropertiesChange(() -> {
             Worker.State state = webEngine.getLoadWorker().getState();
-            if (state == Worker.State.READY) {
+            if (state == Worker.State.READY) { // happens on first view, but also on subsequent views in the browser when the user navigates back here (as the browser unloads the iFrame each time it's removed from the DOM)
                 String GOOGLE_MAP_JS_API_KEY = SourcesConfig.getSourcesRootConfig().getString("GOOGLE_MAP_JS_API_KEY");
                 window = (JSObject) webEngine.executeScript("window");
                 window.setMember("java", DynamicMapView.this);
+                googleMarkers.clear(); // Also important to clear googleMarkers on subsequent views, because we need to recreate them all
                 webEngine.executeScript("var googleMap; \n" +
                         "(g=>{var h,a,k,p=\"The Google Maps JavaScript API\",c=\"google\",l=\"importLibrary\",q=\"__ib__\",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement(\"script\"));e.set(\"libraries\",[...r]+\"\");for(k in g)e.set(k.replace(/[A-Z]/g,t=>\"_\"+t[0].toLowerCase()),g[k]);e.set(\"callback\",c+\".maps.\"+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+\" could not load.\"));a.nonce=m.querySelector(\"script[nonce]\")?.nonce||\"\";m.head.append(a)}));d[l]?console.warn(p+\" only loads once. Ignoring:\",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({\n" +
                         "    key: \"" + GOOGLE_MAP_JS_API_KEY + "\",\n" +
