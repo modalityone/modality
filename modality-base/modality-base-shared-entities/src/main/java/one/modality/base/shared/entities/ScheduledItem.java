7b5601326667ffdeb31e496e672ecb4b5756e87c
Bruno Salmon <dev.salmonb@gmail.com>
2024-03-26 11:42:07 +0000

    Added timeline setter/getter on ScheduledItem

--- modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/ScheduledItem.java
+++ modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/ScheduledItem.java
@@ -1,6 +1,7 @@
 package one.modality.base.shared.entities;
 
 import dev.webfx.stack.orm.entity.Entity;
+import dev.webfx.stack.orm.entity.EntityId;
 import one.modality.base.shared.entities.markers.*;
 
 /**
@@ -11,5 +12,18 @@ public interface ScheduledItem extends Entity,
         EntityHasLocalDate,
         EntityHasSiteAndItem,
         EntityHasStartAndEndTime {
+
+    default void setTimeLine(Object timeline) {
+        setForeignField("timeline", timeline);
+    }
+
+    default EntityId getTimelineId() {
+        return getForeignEntityId("timeline");
+    }
+
+    default Timeline getTimeline() {
+        return getForeignEntity("timeline");
+    }
+
 
 }
