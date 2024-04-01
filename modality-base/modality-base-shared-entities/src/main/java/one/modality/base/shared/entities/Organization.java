b543dff7aed3c9125caf8895d12613d7790f2fb9
Bruno Salmon <dev.salmonb@gmail.com>
2024-04-01 10:53:23 +0100

    Added corporation field java mapping on Organization entity

--- modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/Organization.java
+++ modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/Organization.java
@@ -1,10 +1,7 @@
 package one.modality.base.shared.entities;
 
 import dev.webfx.stack.orm.entity.EntityId;
-import one.modality.base.shared.entities.markers.EntityHasCountry;
-import one.modality.base.shared.entities.markers.EntityHasIcon;
-import one.modality.base.shared.entities.markers.EntityHasLabel;
-import one.modality.base.shared.entities.markers.EntityHasName;
+import one.modality.base.shared.entities.markers.*;
 
 /**
  * @author Bruno Salmon
@@ -13,7 +10,8 @@ public interface Organization extends
         EntityHasName,
         EntityHasLabel,
         EntityHasIcon,
-        EntityHasCountry {
+        EntityHasCountry,
+        EntityHasCorporation {
 
     default void setClosed(boolean closed) { setFieldValue("closed", closed); }
 
