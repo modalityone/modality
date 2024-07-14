fb0d6b07f70035128f7ce8f6e204ad62c88a1e7a
Bruno Salmon <dev.salmonb@gmail.com>
2024-07-14 16:30:45 +0100

    Animated Ok button with progressIndicator during execution in DialogExecutorUtil

--- modality-base/modality-base-backoffice-operations-generic/src/main/java/one/modality/base/backoffice/operations/entities/generic/DialogExecutorUtil.java
+++ modality-base/modality-base-backoffice-operations-generic/src/main/java/one/modality/base/backoffice/operations/entities/generic/DialogExecutorUtil.java
@@ -8,6 +8,8 @@ import dev.webfx.stack.ui.controls.dialog.DialogBuilderUtil;
 import dev.webfx.stack.ui.controls.dialog.DialogContent;
 import dev.webfx.stack.ui.dialog.DialogCallback;
 import dev.webfx.stack.ui.exceptions.UserCancellationException;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
@@ -29,17 +31,25 @@ public final class DialogExecutorUtil {
             });
             DialogBuilderUtil.armDialogContentButtons(dialogContent, dialogCallback -> {
                 executing[0] = true;
+                Button executingButton = dialogContent.getOkButton();
+                turnOnButtonWaitMode(executingButton);
                 executor.get()
                     .onFailure(cause -> {
-                        reportException(dialogCallback, parentContainer, cause); // Actually just print stack trace for now...
-                        if (dialogCallback != null) // So we close the window
-                            dialogCallback.closeDialog();
                         promise.fail(cause);
+                        UiScheduler.runInUiThread(() -> {
+                            reportException(dialogCallback, parentContainer, cause); // Actually just print stack trace for now...
+                            if (dialogCallback != null) // So we close the window
+                                dialogCallback.closeDialog();
+                            turnOffButtonWaitMode(executingButton);
+                        });
                     })
                     .onSuccess(b -> {
-                        if (dialogCallback != null)
-                            dialogCallback.closeDialog();
                         promise.complete();
+                        UiScheduler.runInUiThread(() -> {
+                            if (dialogCallback != null)
+                                dialogCallback.closeDialog();
+                            turnOffButtonWaitMode(executingButton);
+                        });
                     });
             });
         });
@@ -51,6 +61,18 @@ public final class DialogExecutorUtil {
             dialogCallback.showException(cause);
         else
             AlertUtil.showExceptionAlert(cause, parentContainer.getScene().getWindow());
+    }
+
+    private static void turnOnButtonWaitMode(Button button) {
+        ProgressIndicator progressIndicator = new ProgressIndicator();
+        progressIndicator.setPrefSize(20, 20); // Note setMaxSize() doesn't work with WebFX but setPrefSize() does
+        button.setDisable(true);
+        button.setGraphic(progressIndicator);
+    }
+
+    private static void turnOffButtonWaitMode(Button button) {
+        button.setDisable(false);
+        button.setGraphic(null);
     }
 
 }
