bc8a4c9562ec8bbc3d349826058f86cbb7774665
Bruno Salmon <dev.salmonb@gmail.com>
2024-09-12 14:25:10 +0100

    Moved DayTemplate getters/setters to Timeline (was wrongly in DayTemplate)

--- modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/DayTemplate.java
+++ modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/DayTemplate.java
@@ -1,23 +1,10 @@
 package one.modality.base.shared.entities;
 
-import dev.webfx.stack.orm.entity.EntityId;
 import one.modality.base.shared.entities.markers.EntityHasEvent;
 import one.modality.base.shared.entities.markers.EntityHasName;
 
 public interface DayTemplate extends
     EntityHasEvent,
     EntityHasName {
-
-    default void setDayTemplate(Object dayTemplate) {
-        setForeignField("dayTemplate", dayTemplate);
-    }
-
-    default EntityId getDayTemplateId() {
-        return getForeignEntityId("dayTemplate");
-    }
-
-    default DayTemplate getDayTemplate() {
-        return getForeignEntity("dayTemplate");
-    }
 
 }
--- modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/Timeline.java
+++ modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/Timeline.java
@@ -1,6 +1,7 @@
 package one.modality.base.shared.entities;
 
 import dev.webfx.stack.orm.entity.Entity;
+import dev.webfx.stack.orm.entity.EntityId;
 import one.modality.base.shared.entities.markers.*;
 
 /**
@@ -12,6 +13,16 @@ public interface Timeline extends Entity,
     EntityHasEvent,
     EntityHasStartAndEndTime {
 
+    default void setDayTemplate(Object dayTemplate) {
+        setForeignField("dayTemplate", dayTemplate);
+    }
 
+    default EntityId getDayTemplateId() {
+        return getForeignEntityId("dayTemplate");
+    }
+
+    default DayTemplate getDayTemplate() {
+        return getForeignEntity("dayTemplate");
+    }
 
 }
