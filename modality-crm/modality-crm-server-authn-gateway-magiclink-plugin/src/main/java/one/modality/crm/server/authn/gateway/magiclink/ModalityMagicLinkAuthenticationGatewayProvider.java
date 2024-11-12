ab048b870ca33b652d894328dbaab959fa642c3a
Bruno Salmon <dev.salmonb@gmail.com>
2024-11-12 15:38:12 +0000

    Changed magic link implementation (move client from app to activity)

--- modality-crm/modality-crm-server-authn-gateway-magiclink-plugin/src/main/java/one/modality/crm/server/authn/gateway/magiclink/ModalityMagicLinkAuthenticationGatewayProvider.java
+++ modality-crm/modality-crm-server-authn-gateway-magiclink-plugin/src/main/java/one/modality/crm/server/authn/gateway/magiclink/ModalityMagicLinkAuthenticationGatewayProvider.java
@@ -9,8 +9,8 @@ import dev.webfx.platform.util.uuid.Uuid;
 import dev.webfx.stack.authn.*;
 import dev.webfx.stack.authn.logout.server.LogoutPush;
 import dev.webfx.stack.authn.server.gateway.spi.ServerAuthenticationGatewayProvider;
-import dev.webfx.stack.mail.MailService;
 import dev.webfx.stack.mail.MailMessage;
+import dev.webfx.stack.mail.MailService;
 import dev.webfx.stack.orm.datasourcemodel.service.DataSourceModelService;
 import dev.webfx.stack.orm.domainmodel.DataSourceModel;
 import dev.webfx.stack.orm.domainmodel.HasDataSourceModel;
@@ -27,6 +27,7 @@ import one.modality.crm.shared.services.authn.ModalityGuestPrincipal;
 import one.modality.crm.shared.services.authn.ModalityUserPrincipal;
 
 import java.time.Clock;
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 /**
@@ -34,7 +35,11 @@ import java.time.LocalDateTime;
  */
 public class ModalityMagicLinkAuthenticationGatewayProvider implements ServerAuthenticationGatewayProvider, HasDataSourceModel {
 
-    private static final String MAGIC_LINK_APP_ROUTE = "/magic-link/#/:lang/:token";
+    private static final boolean SKIP_LINK_VALIDITY_CHECK = false; // Can be set to true when debugging the magic link client
+    private static final Duration LINK_EXPIRATION_DURATION = Duration.ofMinutes(10);
+
+    private static final String MAGIC_LINK_ACTIVITY_PATH_PREFIX = "/magic-link";
+    private static final String MAGIC_LINK_ACTIVITY_PATH_FULL = MAGIC_LINK_ACTIVITY_PATH_PREFIX + "/:token"; //  + "/#/:lang/:token";
 
     private final DataSourceModel dataSourceModel;
 
@@ -67,7 +72,7 @@ public class ModalityMagicLinkAuthenticationGatewayProvider implements ServerAut
             return renewAndSendMagicLink((MagicLinkRenewalRequest) userCredentials);
         if (userCredentials instanceof MagicLinkCredentials)
             return authenticateWithMagicLink((MagicLinkCredentials) userCredentials);
-        return Future.failedFuture(getClass().getSimpleName() + ".authenticate() requires a " + MagicLinkRequest.class.getSimpleName() + " or " + MagicLinkCredentials.class.getSimpleName() + " argument");
+        return Future.failedFuture(getClass().getSimpleName() + ".authenticate() requires a " + MagicLinkRequest.class.getSimpleName() + ", " + MagicLinkRenewalRequest.class.getSimpleName() + " or " + MagicLinkCredentials.class.getSimpleName() + " argument");
     }
 
     private Future<Void> createAndSendMagicLink(MagicLinkRequest request) {
@@ -75,6 +80,7 @@ public class ModalityMagicLinkAuthenticationGatewayProvider implements ServerAut
             ThreadLocalStateHolder.getRunId(), // runId = this runId (runId of the session where the request originates)
             Strings.toSafeString(request.getLanguage()), // lang
             request.getClientOrigin(), // client origin
+            request.getRequestedPath(),
             request.getEmail(),
             request.getContext()
         );
@@ -82,26 +88,31 @@ public class ModalityMagicLinkAuthenticationGatewayProvider implements ServerAut
 
     private Future<Void> renewAndSendMagicLink(MagicLinkRenewalRequest request) {
         return EntityStore.create(dataSourceModel)
-            .<MagicLink>executeQuery("select loginRunId, lang, link, email from MagicLink where token=? order by id desc limit 1", request.getPreviousToken())
+            .<MagicLink>executeQuery("select loginRunId, lang, link, email, successUrl from MagicLink where token=? order by id desc limit 1", request.getPreviousToken())
             .map(Collections::first)
             .compose(magicLink -> {
                 if (magicLink == null)
                     return Future.failedFuture("Magic link token not found");
                 String link = magicLink.getLink();
-                String clientOrigin = link.substring(0, link.indexOf(MAGIC_LINK_APP_ROUTE));
+                String clientOrigin = link.substring(0, link.indexOf(MAGIC_LINK_ACTIVITY_PATH_PREFIX));
                 return storeAndSendMagicLink(
                     magicLink.getLoginRunId(),
                     magicLink.getLang(),
                     clientOrigin,
+                    magicLink.getRequestedPath(),
                     magicLink.getEmail(),
                     null
                 );
             });
     }
 
-    private Future<Void> storeAndSendMagicLink(String loginRunId, String lang, String clientOrigin, String email, Object context) {
+    private Future<Void> storeAndSendMagicLink(String loginRunId, String lang, String clientOrigin, String requestedPath, String email, Object context) {
+        if (!clientOrigin.startsWith("http")) {
+            clientOrigin = (clientOrigin.contains(":80") ? "http" : "https") + clientOrigin.substring(clientOrigin.indexOf("://"));
+        }
         String token = Uuid.randomUuid();
-        String link = clientOrigin + MAGIC_LINK_APP_ROUTE.replace(":token", token).replace(":lang", lang);
+        String link = clientOrigin + hashPath(MAGIC_LINK_ACTIVITY_PATH_FULL.replace(":token", token).replace(":lang", lang));
+        requestedPath = unhashPath(requestedPath);
         UpdateStore updateStore = UpdateStore.create(dataSourceModel);
         MagicLink magicLink = updateStore.insertEntity(MagicLink.class);
         magicLink.setLoginRunId(loginRunId);
@@ -109,6 +120,7 @@ public class ModalityMagicLinkAuthenticationGatewayProvider implements ServerAut
         magicLink.setLang(lang);
         magicLink.setLink(link);
         magicLink.setEmail(email);
+        magicLink.setRequestedPath(requestedPath);
         ModalityContext modalityContext = context instanceof ModalityContext ? (ModalityContext) context
             : new ModalityContext(1 /* default organizationId if no context is provided */, null, null, null);
         return updateStore.submitChanges()
@@ -118,21 +130,23 @@ public class ModalityMagicLinkAuthenticationGatewayProvider implements ServerAut
             });
     }
 
-    private Future<Void> authenticateWithMagicLink(MagicLinkCredentials credentials) {
+    private Future<String> authenticateWithMagicLink(MagicLinkCredentials credentials) {
         String usageRunId = ThreadLocalStateHolder.getRunId();
         // 1) Checking the existence of the magic link in the database, and if so, loading it with required info
         return EntityStore.create(dataSourceModel)
-            .<MagicLink>executeQuery("select loginRunId,email,creationDate,usageDate from MagicLink where token=? limit 1", credentials.getToken())
+            .<MagicLink>executeQuery("select loginRunId,email,creationDate,usageDate,requestedPath from MagicLink where token=? limit 1", credentials.getToken())
             .compose(magicLinks -> {
-                if (magicLinks.isEmpty())
+                MagicLink magicLink = Collections.first(magicLinks);
+                if (magicLink == null)
                     return Future.failedFuture("Magic link not found (token: " + credentials.getToken() + ")");
                 // 2) Checking the magic link is still valid (not already used and not expired)
-                MagicLink magicLink = magicLinks.get(0);
-                if (magicLink.getUsageDate() != null)
-                    return Future.failedFuture("Magic link already used (token: " + credentials.getToken() + ")");
                 LocalDateTime now = LocalDateTime.now(Clock.systemUTC());
-                if (magicLink.getCreationDate() == null || now.isAfter(magicLink.getCreationDate().plusMinutes(10))) {
-                    return Future.failedFuture("Magic link expired (token: " + credentials.getToken() + ")");
+                if (!SKIP_LINK_VALIDITY_CHECK) {
+                    if (magicLink.getUsageDate() != null)
+                        return Future.failedFuture("Magic link already used (token: " + credentials.getToken() + ")");
+                    if (magicLink.getCreationDate() == null || now.isAfter(magicLink.getCreationDate().plus(LINK_EXPIRATION_DURATION))) {
+                        return Future.failedFuture("Magic link expired (token: " + credentials.getToken() + ")");
+                    }
                 }
                 // 3) The magic link is valid, so we memorise its usage date, and also check if the request comes from
                 // a registered or unregistered user (with or without an account)
@@ -140,32 +154,36 @@ public class ModalityMagicLinkAuthenticationGatewayProvider implements ServerAut
                     .<Person>executeQuery("select frontendAccount from Person p where frontendAccount.username=? order by p.id limit 1", magicLink.getEmail())
                     .compose(persons -> {
                         // 4) Preparing the userId = ModalityUserPrincipal for registered users, ModalityGuestPrincipal for unregistered users
-                        String loginRunId = magicLink.getLoginRunId();
-                        Object targetUserId;
+                        Object userId;
                         if (!persons.isEmpty()) {
                             Person userPerson = persons.get(0);
-                            targetUserId = new ModalityUserPrincipal(userPerson.getPrimaryKey(), userPerson.getForeignEntity("frontendAccount").getPrimaryKey());
+                            userId = new ModalityUserPrincipal(userPerson.getPrimaryKey(), userPerson.getForeignEntity("frontendAccount").getPrimaryKey());
                         } else {
-                            targetUserId = new ModalityGuestPrincipal(magicLink.getEmail());
+                            userId = new ModalityGuestPrincipal(magicLink.getEmail());
                         }
-                        // 5) Pushing the userId to the original client from which the magic link request was made.
-                        // The original client is identified by runId. Pushing the userId will cause a login, and
-                        // subsequently a push of the authorizations.
-                        Object targetState = StateAccessor.createUserIdState(targetUserId);
+                        // 5) Pushing the userId to the magic link client which is identified by runId = usageRunId.
+                        // Pushing the userId will cause a login, and subsequently a push of the authorizations.
 
-                        // We push the userId to the original login client directly here, and indirectly this should be
-                        // followed by a subsequent push of the authorizations to that same client (as explained above).
-                        return PushServerService.pushState(targetState, loginRunId)
-                            .compose(ignored -> { // indicates that the original client acknowledged this login push
-                                // 6) Now that we managed to reach the original client and have a successful login,
+                        return PushServerService.pushState(StateAccessor.createUserIdState(userId), usageRunId)
+                            .compose(ignored -> { // indicates that the magic link client acknowledged this login push
+                                // 6) Now that we managed to reach the magic link client and have a successful login,
                                 // we record the usage date in the database. This will indicate that the magic link has
                                 // been used, and can't be reused a second time.
                                 UpdateStore updateStore = UpdateStore.createAbove(magicLink.getStore());
                                 MagicLink ml = updateStore.updateEntity(magicLink);
                                 ml.setUsageDate(now);
                                 ml.setUsageRunId(usageRunId);
-                                return updateStore.submitChanges().map(ignored2 -> (Void) null)
-                                    .onFailure(Console::log);
+                                return updateStore.submitChanges()
+                                    .map(ignored2 -> magicLink.getRequestedPath())
+                                    .onFailure(Console::log)
+                                    .onSuccess(ignored2 -> {
+                                        // 7) We also finally push the userId to the original login client here, and
+                                        // this should be followed by a subsequent push of the authorizations to that
+                                        // same client (as explained above).
+                                        String loginRunId = magicLink.getLoginRunId();
+                                        PushServerService.pushState(StateAccessor.createUserIdState(userId), loginRunId);
+                                    })
+                                    ;
                             });
                     });
             });
@@ -231,6 +249,14 @@ public class ModalityMagicLinkAuthenticationGatewayProvider implements ServerAut
     @Override
     public Future<Void> logout() {
         return LogoutPush.pushLogoutMessageToClient();
+    }
+
+    private static String hashPath(String path) {
+        return path.startsWith("/#") ? path : "/#" + path;
+    }
+
+    private static String unhashPath(String path) {
+        return Strings.removePrefix(path, "/#");
     }
 
 }
