279ae2e13b5c2bb1e514264b72e3bf4875a2f8ce
Bruno Salmon <dev.salmonb@gmail.com>
2024-12-03 11:01:12 +0000

    Added modality-base-client-messaging module with new ModalityMessaging class (initial version)

---
+++ modality-base/modality-base-client-messaging/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module modality.base.client.messaging {
+
+    // Direct dependencies modules
+    requires webfx.platform.async;
+    requires webfx.stack.com.bus;
+    requires webfx.stack.session.state.client.fx;
+
+    // Exported packages
+    exports one.modality.base.client.messaging;
+
+}
