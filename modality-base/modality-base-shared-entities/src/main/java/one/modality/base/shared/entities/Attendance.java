5a47f855265bcc9bbe3175231e2b5e932eeca78e
Bruno Salmon <dev.salmonb@gmail.com>
2024-12-04 13:11:06 +0000

    Added attended field to Attendance entity and domain class

--- modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/Attendance.java
+++ modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/Attendance.java
@@ -9,8 +9,25 @@ import dev.webfx.stack.orm.entity.Entity;
  * @author Bruno Salmon
  */
 public interface Attendance extends Entity, EntityHasDocumentLine, EntityHasLocalDate {
+    String attended = "attended";
     String scheduledResource = "scheduledResource";
     String scheduledItem = "scheduledItem";
+
+    default Boolean isAttended() {
+        return getBooleanFieldValue(attended);
+    }
+
+    default void setAttended(Boolean value) {
+        setFieldValue(attended, value);
+    }
+
+    default void setScheduledResource(Object value) {
+        setForeignField(scheduledResource, value);
+    }
+
+    default EntityId getScheduledResourceId() {
+        return getForeignEntityId(scheduledResource);
+    }
 
     default ScheduledResource getScheduledResource() {
         return getForeignEntity(scheduledResource);
