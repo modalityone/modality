5cbf642fd27cef69d78f2780ca960c43f81aa984
David <david.dev.kbs@kadampa.net>
2024-12-10 15:12:19 +0000

    Added I18n Keys for Authn centralised code.

---
+++ modality-crm/modality-crm-shared-authn/src/main/java/one/modality/crm/shared/services/authn/ModalityAuthenticationI18nKeys.java
@@ -0,0 +1,18 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+package one.modality.crm.shared.services.authn;
+
+public interface ModalityAuthenticationI18nKeys {
+
+    String EmailAccountAlreadyExistError = "EmailAccountAlreadyExistError";
+    String EmailTokenAlreadyUsedError = "EmailTokenAlreadyUsedError";
+    String EmailTokenExpiredError = "EmailTokenExpiredError";
+    String EmailTokenUnexpectedError = "EmailTokenUnexpectedError";
+    String EmailTokenUnrecognizedError = "EmailTokenUnrecognizedError";
+    String MagicLinkUnrecognisedError = "MagicLinkUnrecognisedError";
+    String MagicLinkAlreadyUsedError = "MagicLinkAlreadyUsedError";
+    String MagicLinkExpiredError = "MagicLinkExpiredError";
+    String MagicLinkPushError = "MagicLinkPushError";
+    String MagicLinkBusClosedError = "MagicLinkBusClosedError";
+    String MagicLinkUnexpectedError = "MagicLinkUnexpectedError";
+
+}
