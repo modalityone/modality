dd1104426c10390bb1a2034e21de107a70af45ea
Bruno Salmon <dev.salmonb@gmail.com>
2024-12-10 12:03:00 +0000

    Removed unnecessary RouteToMagicLinkRequest

--- modality-crm/modality-crm-client-activity-magiclink-plugin/src/main/java/one/modality/crm/activities/magiclink/MagicLinkRouting.java
+++ modality-crm/modality-crm-client-activity-magiclink-plugin/src/main/java/one/modality/crm/activities/magiclink/MagicLinkRouting.java
@@ -1,21 +1,14 @@
 package one.modality.crm.activities.magiclink;
 
-import dev.webfx.platform.windowhistory.spi.BrowsingHistory;
 import dev.webfx.stack.orm.domainmodel.activity.viewdomain.impl.ViewDomainActivityContextFinal;
-import dev.webfx.stack.routing.router.auth.authz.RouteRequest;
 import dev.webfx.stack.routing.uirouter.UiRoute;
-import dev.webfx.stack.routing.uirouter.activity.uiroute.UiRouteActivityContext;
 import dev.webfx.stack.routing.uirouter.impl.UiRouteImpl;
-import dev.webfx.stack.routing.uirouter.operations.RoutePushRequest;
-import dev.webfx.stack.routing.uirouter.operations.RouteRequestEmitter;
-import dev.webfx.stack.ui.operation.HasOperationCode;
 
 public final class MagicLinkRouting {
 
     static final String PATH_TOKEN_PARAMETER_NAME = "token";
 
     private final static String PATH = "/magic-link/:" + PATH_TOKEN_PARAMETER_NAME;
-    private final static String OPERATION_CODE = "RouteToMagicLink";
 
     public static String getPath() {
         return PATH;
@@ -33,26 +26,6 @@ public final class MagicLinkRouting {
                     , MagicLinkActivity::new
                     , ViewDomainActivityContextFinal::new
             );
-        }
-    }
-
-    public static class RouteToMagicLinkRequest extends RoutePushRequest implements HasOperationCode {
-
-        public RouteToMagicLinkRequest(BrowsingHistory browsingHistory) {
-            super(getPath(), browsingHistory);
-        }
-
-        @Override
-        public Object getOperationCode() {
-            return OPERATION_CODE;
-        }
-    }
-
-    public static class RouteToMagicLinkRequestEmitter implements RouteRequestEmitter {
-
-        @Override
-        public RouteRequest instantiateRouteRequest(UiRouteActivityContext context) {
-            return new RouteToMagicLinkRequest(context.getHistory());
         }
     }
 }
