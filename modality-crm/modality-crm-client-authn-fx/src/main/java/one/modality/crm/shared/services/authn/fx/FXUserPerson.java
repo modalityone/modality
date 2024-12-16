d0fc8599d8d7afdec16cf2f920ef79d383833464
Bruno Salmon <dev.salmonb@gmail.com>
2024-12-16 16:18:56 +0000

    Added reloadUserPerson() method in FXUserPerson

--- modality-crm/modality-crm-client-authn-fx/src/main/java/one/modality/crm/shared/services/authn/fx/FXUserPerson.java
+++ modality-crm/modality-crm-client-authn-fx/src/main/java/one/modality/crm/shared/services/authn/fx/FXUserPerson.java
@@ -5,6 +5,7 @@ import dev.webfx.platform.console.Console;
 import dev.webfx.platform.uischeduler.UiScheduler;
 import dev.webfx.stack.orm.datasourcemodel.service.DataSourceModelService;
 import dev.webfx.stack.orm.domainmodel.DataSourceModel;
+import dev.webfx.stack.orm.entity.EntityId;
 import dev.webfx.stack.orm.entity.EntityStore;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
@@ -18,16 +19,19 @@ public final class FXUserPerson {
     private final static ObjectProperty<Person> userPersonProperty = new SimpleObjectProperty<>();
 
     static {
-        FXProperties.runNowAndOnPropertyChange(userPersonId -> {
-            if (userPersonId == null)
-                setUserPerson(null);
-            else {
-                DataSourceModel dataSourceModel = DataSourceModelService.getDefaultDataSourceModel();
-                EntityStore.create(dataSourceModel).<Person>executeQuery("select firstName,lastName,male,ordained,email,phone,street,postCode,cityName,country,organization from Person where id=?", userPersonId)
-                        .onFailure(Console::log)
-                        .onSuccess(persons -> UiScheduler.runInUiThread(() -> setUserPerson(persons.get(0))));
-            }
-        }, FXUserPersonId.userPersonIdProperty());
+        FXProperties.runNowAndOnPropertyChange(FXUserPerson::reloadUserPerson, FXUserPersonId.userPersonIdProperty());
+    }
+
+    public static void reloadUserPerson() {
+        EntityId userPersonId = FXUserPersonId.getUserPersonId();
+        if (userPersonId == null)
+            setUserPerson(null);
+        else {
+            DataSourceModel dataSourceModel = DataSourceModelService.getDefaultDataSourceModel();
+            EntityStore.create(dataSourceModel).<Person>executeQuery("select firstName,lastName,male,ordained,email,phone,street,postCode,cityName,country,organization from Person where id=?", userPersonId)
+                .onFailure(Console::log)
+                .onSuccess(persons -> UiScheduler.runInUiThread(() -> setUserPerson(persons.get(0))));
+        }
     }
 
     public static Person getUserPerson() {
