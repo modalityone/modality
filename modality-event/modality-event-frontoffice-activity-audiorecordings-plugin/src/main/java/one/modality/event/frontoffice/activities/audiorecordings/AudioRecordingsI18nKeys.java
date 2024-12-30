9f1bd793e4377c3c8d4859dbd78864900805b1b7
David <david.dev.kbs@kadampa.net>
2024-12-30 15:43:39 +0000

    Initial international dictionaries creation

--- modality-event/modality-event-frontoffice-activity-audiorecordings-plugin/src/main/java/one/modality/event/frontoffice/activities/audiorecordings/AudioRecordingsI18nKeys.java
+++ modality-event/modality-event-frontoffice-activity-audiorecordings-plugin/src/main/java/one/modality/event/frontoffice/activities/audiorecordings/AudioRecordingsI18nKeys.java
@@ -3,16 +3,15 @@ package one.modality.event.frontoffice.activities.audiorecordings;
 
 public interface AudioRecordingsI18nKeys {
 
-    String AudioRecordingsMenu = "AudioRecordingsMenu";
-    String AudioRecordingsHeader = "AudioRecordingsHeader";
-    String CheckoutAudioRecordings = "CheckoutAudioRecordings";
     String AudioRecordingNotYetPublished = "AudioRecordingNotYetPublished";
-    String NoAudioRecordingForThisEvent = "NoAudioRecordingForThisEvent";
-    String AvailableUntil = "AvailableUntil";
-    String Play = "Play";
-    String Download = "Download";
+    String CheckoutAudioRecordings = "CheckoutAudioRecordings";
     String ListOfTracks = "ListOfTracks";
     String ContentExpired = "ContentExpired";
-    String Recordings = "Recordings";
+    String AudioRecordingsMenu = "AudioRecordingsMenu";
+    String AudioRecordingsHeader = "AudioRecordingsHeader";
+    String NoAudioRecordingForThisEvent = "NoAudioRecordingForThisEvent";
+    String Download = "Download";
+    String AvailableUntil = "AvailableUntil";
+    String Play = "Play";
 
 }
--- modality-event/modality-event-frontoffice-activity-videos-plugin/src/main/java/one/modality/event/frontoffice/activities/videos/VideosI18nKeys.java
+++ modality-event/modality-event-frontoffice-activity-videos-plugin/src/main/java/one/modality/event/frontoffice/activities/videos/VideosI18nKeys.java
@@ -3,34 +3,34 @@ package one.modality.event.frontoffice.activities.videos;
 
 public interface VideosI18nKeys {
 
-    String VideosMenu = "VideosMenu";
-    String VideosHeader = "VideosHeader";
-    String VideoNotYetPublished = "VideoNotYetPublished";
-    String NoVideosForThisEvent = "NoVideosForThisEvent";
-    String Available = "Available";
-    String Unavailable = "Unavailable";
-    String VideoDelayed = "VideoDelayed";
-    String LivestreamTitle = "LivestreamTitle";
-    String PastRecordings = "PastRecordings";
-    String Expired = "Expired";
-    String EventSchedule = "EventSchedule";
-    String SelectTheDayBelow = "SelectTheDayBelow";
-    String Date = "Date";
-    String Status = "Status";
-    String Name = "Name";
+    String WatchAgain = "WatchAgain";
+    String RecordingSoonAvailable = "RecordingSoonAvailable";
     String TimeZoneUK = "TimeZoneUK";
-    String GMTZoneUK = "GMTZoneUK";
+    String EventSchedule = "EventSchedule";
+    String Name = "Name";
+    String Unavailable = "Unavailable";
+    String LivestreamTitle = "LivestreamTitle";
     String Remarks = "Remarks";
-    String Watch = "Watch";
-    String ViewAllDays = "ViewAllDays";
     String ScheduleForSpecificDate = "ScheduleForSpecificDate";
+    String Watch = "Watch";
+    String Available = "Available";
     String VideoExpiredOn = "VideoExpiredOn";
-    String VideoAvailableUntil = "VideoAvailableUntil";
+    String VideosMenu = "VideosMenu";
+    String LiveNow = "LiveNow";
+    String SelectTheDayBelow = "SelectTheDayBelow";
+    String VideoNotYetPublished = "VideoNotYetPublished";
+    String Status = "Status";
+    String ViewAllDays = "ViewAllDays";
     String VideoExpiredSince = "VideoExpiredSince";
     String OnTime = "OnTime";
-    String LiveNow = "LiveNow";
-    String RecordingSoonAvailable = "RecordingSoonAvailable";
+    String VideoDelayed = "VideoDelayed";
+    String Expired = "Expired";
+    String Date = "Date";
+    String NoVideosForThisEvent = "NoVideosForThisEvent";
     String StartingIn = "StartingIn";
-    String WatchAgain = "WatchAgain";
+    String GMTZoneUK = "GMTZoneUK";
+    String VideosHeader = "VideosHeader";
+    String VideoAvailableUntil = "VideoAvailableUntil";
+    String PastRecordings = "PastRecordings";
 
 }
