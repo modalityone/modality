06224955843afeaf4292f29b27f3e78ffd364ec4
Bruno Salmon <dev.salmonb@gmail.com>
2025-03-08 14:38:56 +0000

    Fixed i18n issue with atTime

--- modality-event/modality-event-client-recurringevents/src/main/java/one/modality/event/client/recurringevents/RecurringEventSchedule.java
+++ modality-event/modality-event-client-recurringevents/src/main/java/one/modality/event/client/recurringevents/RecurringEventSchedule.java
@@ -227,7 +227,7 @@ public class RecurringEventSchedule implements BookableDatesUi {
                     startTime = timeline.getStartTime();
             }
             if (startTime != null) {
-                hourText.setText(I18n.getI18nText(RecurringEventsI18nKeys.AtTime0, startTime.toString()));
+                I18n.bindI18nProperties(hourText, RecurringEventsI18nKeys.AtTime1, startTime.toString());
             }
             scheduledItemBoxes.put(date, this);
             containerVBox.setOnMouseClicked(event -> {
--- modality-event/modality-event-client-recurringevents/src/main/java/one/modality/event/client/recurringevents/RecurringEventsI18nKeys.java
+++ modality-event/modality-event-client-recurringevents/src/main/java/one/modality/event/client/recurringevents/RecurringEventsI18nKeys.java
@@ -3,6 +3,6 @@ package one.modality.event.client.recurringevents;
 
 public interface RecurringEventsI18nKeys {
 
-    String AtTime0 = "AtTime0";
+    String AtTime1 = "AtTime1";
 
 }
