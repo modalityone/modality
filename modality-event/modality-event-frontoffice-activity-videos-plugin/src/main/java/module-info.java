b26abdfa56e8bec5b5ecc860218fd551d63adc71
Bruno Salmon <dev.salmonb@gmail.com>
2025-04-22 16:40:53 +0100

    Ran 'webfx update'

--- modality-event/modality-event-frontoffice-activity-videos-plugin/src/main/java/module-info.java
+++ modality-event/modality-event-frontoffice-activity-videos-plugin/src/main/java/module-info.java
@@ -6,7 +6,6 @@ module modality.event.frontoffice.activity.videos.plugin {
     requires javafx.base;
     requires javafx.controls;
     requires javafx.graphics;
-    requires modality.base.client.bootstrap;
     requires modality.base.client.cloudinary;
     requires modality.base.client.icons;
     requires modality.base.client.messaging;
