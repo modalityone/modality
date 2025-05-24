6d3535fe80f6300eb439a8b5067f12a8e789f26b
Bruno Salmon <dev.salmonb@gmail.com>
2025-05-24 20:41:40 +0100

    Made the select event link toggle the collapse pane (not just expand it)

--- modality-event/modality-event-frontoffice-activity-videostreaming-plugin/src/main/java/one/modality/event/frontoffice/activities/videostreaming/VideoStreamingActivity.java
+++ modality-event/modality-event-frontoffice-activity-videostreaming-plugin/src/main/java/one/modality/event/frontoffice/activities/videostreaming/VideoStreamingActivity.java
@@ -345,7 +345,7 @@ final class VideoStreamingActivity extends ViewDomainActivityBase {
 
         // We display an event selection section only if there are more than 1 event with videos booked by the user
         Hyperlink selectEventLink = I18nControls.newHyperlink("Select another event");
-        selectEventLink.setOnAction(e -> eventsSelectionPane.expand());
+        selectEventLink.setOnAction(e -> eventsSelectionPane.toggleCollapse());
         VBox eventsSelectionVBox = new VBox(10, selectEventLink, eventsSelectionPane);
         Layouts.setMinMaxHeightToPref(eventsSelectionVBox); // No need to compute min/max height as different to pref (layout computation optimization)
         eventsSelectionVBox.setAlignment(Pos.CENTER);
