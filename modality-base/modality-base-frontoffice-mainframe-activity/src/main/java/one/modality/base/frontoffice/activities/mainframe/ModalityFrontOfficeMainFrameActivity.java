116dc25ae7c4ec486e5667964015b783c67db1e9
Bruno Salmon <dev.salmonb@gmail.com>
2025-07-04 20:39:25 +0100

    Fixed wrong computation for the mount node min height in ModalityFrontOfficeMainFrameActivity

--- modality-base/modality-base-frontoffice-mainframe-activity/src/main/java/one/modality/base/frontoffice/activities/mainframe/ModalityFrontOfficeMainFrameActivity.java
+++ modality-base/modality-base-frontoffice-mainframe-activity/src/main/java/one/modality/base/frontoffice/activities/mainframe/ModalityFrontOfficeMainFrameActivity.java
@@ -269,8 +269,8 @@ public final class ModalityFrontOfficeMainFrameActivity extends ModalityClientMa
             if (mountNode != null && getMountNodeEmbeddingScrollPane(mountNode) == null) {
                 if (mountNode instanceof Region mountRegion) {
                     FXProperties.runNowAndOnPropertiesChange(() ->
-                            mountRegion.setMinHeight(pageTransitionPane.getMinHeight() - mainMenuButtonBar.getHeight() - languageMenuBar.getHeight())
-                        , pageTransitionPane.minHeightProperty(), mainMenuButtonBar.heightProperty(), languageMenuBar.heightProperty());
+                            mountRegion.setMinHeight(pageTransitionPane.getMinHeight() - languageMenuBar.getHeight() - mainMenuButtonBar.getHeight() - userMenuButtonBar.getHeight() - footer.getLayoutBounds().getHeight() - VBox.getMargin(footer).getBottom())
+                        , pageTransitionPane.minHeightProperty(), languageMenuBar.heightProperty(), mainMenuButtonBar.heightProperty(), userMenuButtonBar.heightProperty(), footer.layoutBoundsProperty());
                 }
                 registerMountNodeEmbeddingScrollPane(mountNode, scrollPane);
             }
