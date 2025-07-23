d7d4fdb1f43110d4ba4e36d778b886e33f4b9700
Bruno Salmon <dev.salmonb@gmail.com>
2025-07-23 11:52:01 +0100

    Added Terms and conditions i18n messages for the booking form

--- modality-ecommerce/modality-ecommerce-frontoffice-bookingform/src/main/java/one/modality/ecommerce/frontoffice/bookingform/BookingFormI18nKeys.java
+++ modality-ecommerce/modality-ecommerce-frontoffice-bookingform/src/main/java/one/modality/ecommerce/frontoffice/bookingform/BookingFormI18nKeys.java
@@ -4,13 +4,16 @@ package one.modality.ecommerce.frontoffice.bookingform;
 public interface BookingFormI18nKeys {
 
     Object AnyRequest = "AnyRequest";
+    Object TermsAndConditions = "TermsAndConditions";
     Object PayNow1 = "PayNow1";
     Object SummaryTopMessage = "SummaryTopMessage";
     Object PaymentTopMessage = "PaymentTopMessage";
+    Object AgreePrivacyNotice = "AgreePrivacyNotice";
     Object SelectPaymentAmount = "SelectPaymentAmount";
     Object Payment = "Payment";
     Object wholeEvent = "wholeEvent";
     Object SaveBooking = "SaveBooking";
+    Object AgreeTermsAndConditions = "AgreeTermsAndConditions";
     Object PaymentBottomMessage = "PaymentBottomMessage";
     Object SummarySubTopMessage = "SummarySubTopMessage";
     Object Summary = "Summary";
