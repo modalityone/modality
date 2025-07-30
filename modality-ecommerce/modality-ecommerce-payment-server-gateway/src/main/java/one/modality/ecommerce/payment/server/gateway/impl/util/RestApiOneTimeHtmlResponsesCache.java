20b862d748fff84058de473ba2349718fefef2f7
Bruno Salmon <dev.salmonb@gmail.com>
2025-07-30 14:14:57 +0100

    Temporarily disabled file cache in RestApiOneTimeHtmlResponsesCache

--- modality-ecommerce/modality-ecommerce-payment-server-gateway/src/main/java/one/modality/ecommerce/payment/server/gateway/impl/util/RestApiOneTimeHtmlResponsesCache.java
+++ modality-ecommerce/modality-ecommerce-payment-server-gateway/src/main/java/one/modality/ecommerce/payment/server/gateway/impl/util/RestApiOneTimeHtmlResponsesCache.java
@@ -15,18 +15,23 @@ import java.util.Map;
  */
 public final class RestApiOneTimeHtmlResponsesCache {
 
+    // In-memory cache
+    private final static Map<String, String> ONE_TIME_HTML_RESPONSES = new HashMap<>();
+    // And also file cache
     private static final Path PAYMENT_CACHE_DIRECTORY_PATH = Paths.get(System.getProperty("user.home"), ".modality", "payment-cache");
     static {
-        PAYMENT_CACHE_DIRECTORY_PATH.toFile().mkdirs();
+        PAYMENT_CACHE_DIRECTORY_PATH.toFile().mkdirs(); // Ensuring the directory is created if doesn't exist yet
     }
 
-    private final static Map<String, String> ONE_TIME_HTML_RESPONSES = new HashMap<>();
+    private static Path getPaymentCacheFilePath(String key) {
+        return PAYMENT_CACHE_DIRECTORY_PATH.resolve(key);
+    }
 
     public static void registerOneTimeHtmlResponse(String key, String value) {
-        Console.log("[PAYMENT] cache set with key: " + key);
+        Console.log("[PAYMENT] Cache set with key: " + key);
         ONE_TIME_HTML_RESPONSES.put(key, value);
         try {
-            Path paymentCacheFile = Files.createFile(PAYMENT_CACHE_DIRECTORY_PATH.resolve(key));
+            Path paymentCacheFile = Files.createFile(getPaymentCacheFilePath(key));
             BufferedWriter writer = Files.newBufferedWriter(paymentCacheFile, StandardCharsets.UTF_8);
             writer.write(value);
             writer.flush();
@@ -39,14 +44,14 @@ public final class RestApiOneTimeHtmlResponsesCache {
     public static String getOneTimeHtmlResponse(String key) {
         String result = ONE_TIME_HTML_RESPONSES.remove(key);
         try {
-            Path paymentCacheFile = PAYMENT_CACHE_DIRECTORY_PATH.resolve(key);
+            Path paymentCacheFile = getPaymentCacheFilePath(key);
             if (result == null)
                 result = new String(Files.readAllBytes(paymentCacheFile));
-            Files.delete(paymentCacheFile);
+            //Files.delete(paymentCacheFile); // File cache deletion is temporarily disabled for now
         } catch (Exception e) {
             Console.log("[PAYMENT] Error while reading cache key: " + key + ": " + e.getMessage());
         }
-        Console.log("Payment cache requested with key: " + key + " -> " + (result != null ? "found" : "not found"));
+        Console.log("[PAYMENT] Cache requested with key: " + key + " -> " + (result != null ? "found" : "not found"));
         return result; // Removed from the cache when requested (one-time cache)
     }
 }
