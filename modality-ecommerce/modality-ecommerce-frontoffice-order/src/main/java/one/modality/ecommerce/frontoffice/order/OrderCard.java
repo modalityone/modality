3a45a1c5d7df2246ab2f485ebdb0f1ec30075d59
Bruno Salmon <dev.salmonb@gmail.com>
2025-08-01 14:39:19 +0100

    Extracted OrderActions utility class from OrderCard

--- modality-ecommerce/modality-ecommerce-frontoffice-order/src/main/java/one/modality/ecommerce/frontoffice/order/OrderCard.java
+++ modality-ecommerce/modality-ecommerce-frontoffice-order/src/main/java/one/modality/ecommerce/frontoffice/order/OrderCard.java
@@ -6,12 +6,9 @@ import dev.webfx.extras.i18n.spi.impl.I18nSubKey;
 import dev.webfx.extras.operation.OperationUtil;
 import dev.webfx.extras.panes.CollapsePane;
 import dev.webfx.extras.panes.MonoPane;
-import dev.webfx.extras.panes.ScalePane;
 import dev.webfx.extras.styles.bootstrap.Bootstrap;
 import dev.webfx.extras.time.format.LocalizedTime;
 import dev.webfx.extras.util.control.Controls;
-import dev.webfx.extras.util.dialog.DialogCallback;
-import dev.webfx.extras.util.dialog.DialogUtil;
 import dev.webfx.extras.util.layout.Layouts;
 import dev.webfx.extras.util.scene.SceneUtil;
 import dev.webfx.kit.util.properties.FXProperties;
@@ -19,34 +16,25 @@ import dev.webfx.kit.util.properties.ObservableLists;
 import dev.webfx.platform.async.Future;
 import dev.webfx.platform.console.Console;
 import dev.webfx.platform.uischeduler.UiScheduler;
-import dev.webfx.platform.windowhistory.WindowHistory;
 import dev.webfx.stack.orm.entity.Entities;
 import dev.webfx.stack.orm.entity.EntityStoreQuery;
-import dev.webfx.stack.orm.entity.UpdateStore;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import one.modality.base.client.i18n.BaseI18nKeys;
-import one.modality.base.client.mainframe.fx.FXMainFrameDialogArea;
-import one.modality.base.frontoffice.utility.browser.BrowserUtil;
-import one.modality.base.shared.entities.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import one.modality.base.shared.entities.Document;
+import one.modality.base.shared.entities.DocumentLine;
+import one.modality.base.shared.entities.Event;
+import one.modality.base.shared.entities.EventState;
 import one.modality.base.shared.entities.formatters.EventPriceFormatter;
-import one.modality.crm.shared.services.authn.fx.FXUserPerson;
 import one.modality.ecommerce.client.i18n.EcommerceI18nKeys;
-import one.modality.ecommerce.client.workingbooking.WorkingBooking;
-import one.modality.ecommerce.document.service.DocumentAggregate;
-import one.modality.ecommerce.document.service.DocumentService;
-import one.modality.ecommerce.document.service.PolicyAggregate;
 import one.modality.ecommerce.frontoffice.bookingelements.BookingElements;
 import one.modality.event.client.lifecycle.EventLifeCycle;
 
@@ -55,11 +43,12 @@ import java.time.format.DateTimeFormatter;
 
 /**
  * @author David Hello
+ * @author Bruno Salmon
  */
 public final class OrderCard {
 
     // Required fields for retrieving order and its details
-    private static final String ORDER_EVENT_REQUIRED_FIELDS = "event.(name,label,state,image.url,live,startDate,endDate,kbs3, venue.(name,label,country),organization.country)";
+    private static final String ORDER_EVENT_REQUIRED_FIELDS = "event.(name,label,state,image.url,live,startDate,endDate,kbs3,venue.(name,label,country),organization.country)";
     private static final String ORDER_PERSON_REQUIRED_FIELDS = "ref,person,person_firstName,person_lastName,cart.uuid";
     private static final String ORDER_STATUS_REQUIRED_FIELDS = OrderStatus.BOOKING_REQUIRED_FIELDS;
     public static final String ORDER_REQUIRED_FIELDS = ORDER_EVENT_REQUIRED_FIELDS + "," + ORDER_PERSON_REQUIRED_FIELDS + "," + ORDER_STATUS_REQUIRED_FIELDS;
@@ -77,16 +66,15 @@ public final class OrderCard {
     private final Label paidValue = BookingElements.createPriceLabel();
     private final Label remainingLabel = I18nControls.newLabel(I18nKeys.appendColons(EcommerceI18nKeys.RemainingAmount));
     private final Label remainingValue = new Label();
-    private final Label contactUsLabel = Bootstrap.strong(Bootstrap.textPrimary(I18nControls.newLabel(OrderI18nKeys.ContactUsAboutThisBooking)));
-    private final Button modifyOrderButton = Bootstrap.secondaryButton(I18nControls.newButton(OrderI18nKeys.AddOrEditOption));
-    private final Button makePaymentButton = Bootstrap.primaryButton(I18nControls.newButton(OrderI18nKeys.MakePayment));
-    private final Button askRefundButton = Bootstrap.primaryButton(I18nControls.newButton(OrderI18nKeys.AskARefund));
-    private final Button legacyCartButton = Bootstrap.primaryButton(I18nControls.newButton(OrderI18nKeys.LegacyCart));
-    private final Label cancelOrderLabel = Bootstrap.textDanger(I18nControls.newLabel(OrderI18nKeys.CancelBooking));
+    private final Label contactUsLabel;
+    private final Button modifyOrderButton;
+    private final Button makePaymentButton;
+    private final Button askRefundButton;
+    private final Button legacyCartButton;
+    private final Label cancelOrderLabel;
     private final Label viewHideDetailsLabel = Bootstrap.h4(Bootstrap.textPrimary(new Label()));
     private final CollapsePane detailsCollapsePane = new CollapsePane();
 
-    private int remainingAmount; // computed in update UI
     private boolean orderDetailsLoaded = false;
 
     // Public API
@@ -94,6 +82,12 @@ public final class OrderCard {
     // This constructor is called by OrdersView and will be part of a list (ex: upcoming orders or past orders)
     public OrderCard(Document orderDocument) {
         this.orderDocument = orderDocument;
+        contactUsLabel = OrderActions.newContactUsLabel(orderDocument);
+        modifyOrderButton = OrderActions.newModifyOrderButton(orderDocument);
+        makePaymentButton = OrderActions.newMakePaymentButton(orderDocument);
+        askRefundButton = OrderActions.newAskRefundButton(orderDocument);
+        legacyCartButton = OrderActions.newLegacyCartButton(orderDocument);
+        cancelOrderLabel = OrderActions.newCancelOrderLabel(orderDocument, this::loadFromDatabase);
         // Building the UI
         buildUi();
         // First update UI (later update will occur when orderDocumentLines are loaded)
@@ -147,7 +141,7 @@ public final class OrderCard {
         // Updating prices
         int totalPriceNet = orderDocument.getPriceNet();
         int deposit = orderDocument.getPriceDeposit();
-        remainingAmount = totalPriceNet - deposit;
+        int remainingAmount = totalPriceNet - deposit;
         Event event = orderDocument.getEvent();
         orderPriceLabel.setText(EventPriceFormatter.formatWithCurrency(totalPriceNet, event));
         totalValue.setText(EventPriceFormatter.formatWithCurrency(totalPriceNet, event));
@@ -208,8 +202,6 @@ public final class OrderCard {
         // We embed the label in a stretchable mono pane so it appears horizontally centered
         MonoPane viewHideDetailsPane = new MonoPane(viewHideDetailsLabel);
         viewHideDetailsPane.setMaxWidth(Double.MAX_VALUE);
-
-        setupContactUsLabel();
 
         VBox orderDetailsAndSummary = new VBox(
             orderDetails,
@@ -313,12 +305,6 @@ public final class OrderCard {
     }
 
     private Node createOrderActionsBar() {
-        setupModifyOrderButton();
-        setupMakePaymentButton();
-        setupLegacyCartButton();
-        setupAskRefundButton();
-        setupCancelOrderButton();
-
         HBox orderActionsBar = new HBox(12, // Gap of 12 px
             modifyOrderButton, makePaymentButton, legacyCartButton, askRefundButton, Layouts.createHGrowable(), cancelOrderLabel);
         orderActionsBar.setPadding(new Insets(20, 0, 0, 0));
@@ -330,178 +316,4 @@ public final class OrderCard {
         return orderActionsBar;
     }
 
-    // Button setups
-
-    private void setupContactUsLabel() {
-        setupLabeled(contactUsLabel, true, e -> {
-            ContactUsDialog contactUsWindow = new ContactUsDialog();
-            contactUsWindow.buildUI();
-            DialogCallback messageWindowCallback = DialogUtil.showModalNodeInGoldLayout(contactUsWindow.getContainer(), FXMainFrameDialogArea.getDialogArea());
-            contactUsWindow.getCancelButton().setOnMouseClicked(m -> messageWindowCallback.closeDialog());
-
-            contactUsWindow.getSendButton().setOnAction(ae -> {
-                Document d = orderDocument;
-                UpdateStore updateStore = UpdateStore.createAbove(d.getStore());
-                Mail email = updateStore.insertEntity(Mail.class);
-                email.setFromName(d.getFirstName() + ' ' + d.getLastName());
-                email.setFromEmail(d.getEmail());
-                email.setSubject("[" + Entities.getPrimaryKey(d.getEvent()) + "-" + d.getRef() + "] " + contactUsWindow.getSubject());
-                email.setOut(false);
-                email.setDocument(d);
-                String content = contactUsWindow.getMessage();
-                content = content.replaceAll("\r", "<br/>");
-                content = content.replaceAll("\n", "<br/>");
-                content = "<html>" + content + "</html>";
-                email.setContent(content);
-                History history = updateStore.insertEntity(History.class);
-                Person userPerson = FXUserPerson.getUserPerson();
-                if (userPerson != null) {
-                    history.setUserPerson(userPerson);
-                } else
-                    history.setUsername("online");
-                history.setComment("Sent " + email.getSubject());
-                history.setDocument(d);
-                history.setMail(email);
-
-                OperationUtil.turnOnButtonsWaitModeDuringExecution(
-                    updateStore.submitChanges()
-                        .onFailure(Console::log)
-                        .onComplete(c -> contactUsWindow.displaySuccessMessage(5000, messageWindowCallback::closeDialog)),
-                    contactUsWindow.getSendButton(), contactUsWindow.getCancelButton());
-            });
-        });
-    }
-
-    private void setupModifyOrderButton() {
-        setupButton(modifyOrderButton, false, e ->
-            WindowHistory.getProvider().push("/modify-order/" + orderDocument.getPrimaryKey()));
-    }
-
-    private void setupMakePaymentButton() {
-        setupButton(makePaymentButton, false, e ->
-            WindowHistory.getProvider().push("/pay-order/" + orderDocument.getPrimaryKey()));
-    }
-
-    private void setupLegacyCartButton() {
-        legacyCartButton.setGraphicTextGap(10);
-        setupButton(legacyCartButton, false,e ->
-            BrowserUtil.openExternalBrowser(EventLifeCycle.getKbs2BookingCartUrl(orderDocument)));
-    }
-
-    private void setupAskRefundButton() {
-        askRefundButton.setOnAction(e -> {
-            Event event = orderDocument.getEvent();
-            String formattedPrice = EventPriceFormatter.formatWithCurrency(remainingAmount, event);
-            RefundDialog refundWindow = new RefundDialog(formattedPrice, String.valueOf(orderDocument.getRef()), event);
-            refundWindow.buildUI();
-            DialogCallback messageWindowCallback = DialogUtil.showModalNodeInGoldLayout(refundWindow.getContainer(), FXMainFrameDialogArea.getDialogArea());
-            refundWindow.getCancelButton().setOnMouseClicked(m -> messageWindowCallback.closeDialog());
-            refundWindow.getRefundButton().setOnAction(m -> {
-                Document d = orderDocument;
-                UpdateStore updateStore = UpdateStore.createAbove(d.getStore());
-                Mail email = updateStore.insertEntity(Mail.class);
-                email.setFromName(d.getFirstName() + ' ' + d.getLastName());
-                email.setFromEmail(d.getEmail());
-                email.setSubject("[" + d.getPrimaryKey() + "-" + d.getRef() + "] Refund of " + formattedPrice + " requested");
-                email.setOut(false);
-                email.setDocument(d);
-                String content = "The user has requested a refund for his canceled booking. Amount : " + formattedPrice;
-                content = content.replaceAll("\r", "<br/>");
-                content = content.replaceAll("\n", "<br/>");
-                content = "<html>" + content + "</html>";
-                email.setContent(content);
-                History history = updateStore.insertEntity(History.class);
-                history.setUsername("online");
-                history.setComment("Sent " + email.getSubject());
-                history.setDocument(d);
-                history.setMail(email);
-
-                //TODO: prevent the Refund to display if the refund as already been requested, and display somewhere in the interface that the refund has been requested
-                OperationUtil.turnOnButtonsWaitModeDuringExecution(
-                    updateStore.submitChanges()
-                        .onFailure(Console::log)
-                        .onComplete(c -> refundWindow.displayRefundSuccessMessage(8000, messageWindowCallback::closeDialog)),
-                    refundWindow.getRefundButton(), refundWindow.getDonateButton(), refundWindow.getCancelButton());
-
-            });
-            refundWindow.getDonateButton().setOnAction(ae -> {
-                UpdateStore updateStore = UpdateStore.createAbove(orderDocument.getStore());
-                //TODO implementation
-                OperationUtil.turnOnButtonsWaitModeDuringExecution(
-                    updateStore.submitChanges()
-                        .onFailure(Console::log)
-                        .onComplete(c -> refundWindow.displayDonationSuccessMessage(8000, messageWindowCallback::closeDialog)),
-                    refundWindow.getRefundButton(), refundWindow.getDonateButton());
-
-            });
-        });
-    }
-
-    private void setupCancelOrderButton() {
-        cancelOrderLabel.setCursor(Cursor.HAND);
-        cancelOrderLabel.setWrapText(true);
-        cancelOrderLabel.setOnMouseClicked(e -> {
-            BorderPane errorDialog = new BorderPane();
-            errorDialog.setMinWidth(500);
-            ScalePane errorContainer = new ScalePane(errorDialog);
-
-            errorDialog.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(16), Insets.EMPTY)));
-            errorDialog.setPadding(new Insets(30));
-
-            Label title = Bootstrap.strong(Bootstrap.textPrimary(Bootstrap.h3(I18nControls.newLabel(OrderI18nKeys.CancelBookingTitle))));
-            title.setPadding(new Insets(20, 0, 0, 0));
-            title.setWrapText(true);
-            errorDialog.setTop(title);
-            BorderPane.setAlignment(title, Pos.CENTER);
-            Label areYouSureLabel = Bootstrap.strong(Bootstrap.textSecondary(Bootstrap.h5(I18nControls.newLabel(OrderI18nKeys.CancelBookingAreYouSure))));
-            areYouSureLabel.setWrapText(true);
-
-            VBox content = new VBox(30, areYouSureLabel);
-            content.setAlignment(Pos.CENTER);
-            BorderPane.setAlignment(content, Pos.CENTER);
-            BorderPane.setMargin(content, new Insets(30, 0, 30, 0));
-            errorDialog.setCenter(content);
-
-            Label cancelLabelText = Bootstrap.strong(Bootstrap.textSecondary(I18nControls.newLabel(BaseI18nKeys.Cancel)));
-            cancelLabelText.setCursor(Cursor.HAND);
-            DialogCallback errorMessageCallback = DialogUtil.showModalNodeInGoldLayout(errorContainer, FXMainFrameDialogArea.getDialogArea());
-            cancelLabelText.setOnMouseClicked(e2 -> errorMessageCallback.closeDialog());
-
-            Button confirmButton = Bootstrap.largeDangerButton(I18nControls.newButton(BaseI18nKeys.Confirm));
-            confirmButton.setOnAction(ae ->
-                OperationUtil.turnOnButtonsWaitModeDuringExecution(
-                    DocumentService.loadDocumentWithPolicy(orderDocument)
-                        .compose(policyAndDocumentAggregates -> {
-                            PolicyAggregate policyAggregate = policyAndDocumentAggregates.getPolicyAggregate(); // never null
-                            DocumentAggregate existingBooking = policyAndDocumentAggregates.getDocumentAggregate(); // might be null
-                            WorkingBooking workingBooking = new WorkingBooking(policyAggregate, existingBooking);
-                            workingBooking.cancelBooking();
-                            return workingBooking.submitChanges("Booking canceled online by user")
-                                .compose(result -> loadFromDatabase());
-                        })
-                        .onFailure(Console::log)
-                        .onComplete(ar -> {
-                            // Close the dialog only after the operation completes (success or failure)
-                            errorMessageCallback.closeDialog();
-                        }), confirmButton, cancelLabelText));
-            HBox buttonsHBox = new HBox(70, cancelLabelText, confirmButton);
-            buttonsHBox.setPadding(new Insets(30, 20, 20, 20));
-            buttonsHBox.setAlignment(Pos.CENTER);
-            errorDialog.setBottom(buttonsHBox);
-        });
-    }
-
-    private static void setupLabeled(Labeled button, boolean wrap, EventHandler<MouseEvent> onMouseClicked) {
-        button.setCursor(Cursor.HAND);
-        if (wrap)
-            button.setWrapText(true);
-        else
-            button.setMinWidth(Region.USE_PREF_SIZE);
-        button.setOnMouseClicked(onMouseClicked);
-    }
-
-    private static void setupButton(ButtonBase button, boolean wrap, EventHandler<ActionEvent> onAction) {
-        setupLabeled(button, wrap, null);
-        button.setOnAction(onAction);
-    }
 }
