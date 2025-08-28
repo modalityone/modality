6304331b031f7cd9830c2a6019e8793656024802
Bruno Salmon <dev.salmonb@gmail.com>
2025-08-28 11:08:49 +0100

    Added de, es and pt dictionaries in modality-ecommerce-client-i18n module

--- modality-ecommerce/modality-ecommerce-client-i18n/src/main/java/one/modality/ecommerce/client/i18n/EcommerceI18nKeys.java
+++ modality-ecommerce/modality-ecommerce-client-i18n/src/main/java/one/modality/ecommerce/client/i18n/EcommerceI18nKeys.java
@@ -3,28 +3,28 @@ package one.modality.ecommerce.client.i18n;
 
 public interface EcommerceI18nKeys {
 
-    Object MinDeposit = "MinDeposit";
     Object Deposit = "Deposit";
-    Object Balance = "Balance";
-    Object Fee = "Fee";
-    Object Method = "Method";
-    Object olMethod = "olMethod";
-    Object cashMethod = "cashMethod";
-    Object chqMethod = "chqMethod";
-    Object ccMethod = "ccMethod";
     Object btMethod = "btMethod";
-    Object contraMethod = "contraMethod";
-    Object Amount = "Amount";
-    Object minDeposit = "minDeposit";
-    Object TotalAmount = "TotalAmount";
-    Object RemainingAmount = "RemainingAmount";
-    Object Total = "Total";
-    Object Price = "Price";
-    Object TotalPrice = "TotalPrice";
-    Object Pay = "Pay";
-    Object payInFull = "payInFull";
-    Object Paid = "Paid";
     Object Discount = "Discount";
+    Object MinDeposit = "MinDeposit";
+    Object Fee = "Fee";
+    Object Amount = "Amount";
+    Object ccMethod = "ccMethod";
+    Object Pay = "Pay";
+    Object Paid = "Paid";
+    Object payInFull = "payInFull";
+    Object Method = "Method";
+    Object TotalAmount = "TotalAmount";
+    Object olMethod = "olMethod";
+    Object chqMethod = "chqMethod";
+    Object Price = "Price";
+    Object Total = "Total";
+    Object TotalPrice = "TotalPrice";
+    Object contraMethod = "contraMethod";
+    Object cashMethod = "cashMethod";
     Object Invoiced = "Invoiced";
+    Object minDeposit = "minDeposit";
+    Object Balance = "Balance";
+    Object RemainingAmount = "RemainingAmount";
 
 }
