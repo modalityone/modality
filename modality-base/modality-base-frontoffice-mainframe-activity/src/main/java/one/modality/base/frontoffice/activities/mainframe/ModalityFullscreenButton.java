1b43b9de9a70c88edd1b62356b79d277f1c7529b
Bruno Salmon <dev.salmonb@gmail.com>
2025-09-24 17:49:10 +0100

    Removed FullscreenButtonOverlay - now managed in the global ModalityFullscreenButton

--- modality-base/modality-base-frontoffice-mainframe-activity/src/main/java/one/modality/base/frontoffice/activities/mainframe/ModalityFullscreenButton.java
+++ modality-base/modality-base-frontoffice-mainframe-activity/src/main/java/one/modality/base/frontoffice/activities/mainframe/ModalityFullscreenButton.java
@@ -5,9 +5,13 @@ import dev.webfx.extras.player.Player;
 import dev.webfx.extras.util.animation.Animations;
 import dev.webfx.kit.util.properties.FXProperties;
 import dev.webfx.kit.util.properties.Unregisterable;
+import dev.webfx.platform.scheduler.Scheduled;
+import dev.webfx.platform.uischeduler.UiScheduler;
 import javafx.animation.Timeline;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import one.modality.base.client.mainframe.fx.FXMainFrameOverlayArea;
@@ -21,13 +25,21 @@ public class ModalityFullscreenButton {
     private static final Pane FULLSCREEN_BUTTON = MediaButtons.createFullscreenButton();
     private static Timeline FULLSCREEN_BUTTON_TIMELINE;
     private static Unregisterable FULLSCREEN_LAYOUT;
+    private static boolean FULLSCREEN_BUTTON_IN_PLAYER;
+    private static boolean FULLSCREEN_BUTTON_ANIMATED;
+    private static Scheduled FADE_OUT_SCHEDULED;
+    private static final EventHandler<MouseEvent> MOUSE_MOVE_OVER_PLAYER_WITH_FULLSCREEN_BUTTON_HANDLER = e -> {
+        if (FULLSCREEN_BUTTON_TIMELINE == null || Animations.isTimelineFinished(FULLSCREEN_BUTTON_TIMELINE))
+            fadeFullscreenButton(true);
+        fadeFullscreenButton(false);
+    };
 
     public static void setupModalityFullscreenButton() {
         FullscreenButton.setupFullscreenButton(
             ModalityFullscreenButton::showFullscreenButton,
             ModalityFullscreenButton::hideFullscreenButton,
-            MediaButtons::animateFullscreenButton
-            );
+            ModalityFullscreenButton::animateFullscreenButton
+        );
     }
 
     private static Pane showFullscreenButton() {
@@ -36,7 +48,8 @@ public class ModalityFullscreenButton {
         ObservableList<Node> overlayChildren;
         Region overlayArea;
         Player playingPlayer = FullscreenButton.getSuitableFullscreenPlayingPlayer();
-        if (playingPlayer != null && playingPlayer.appRequestedOverlayChildren()) {
+        FULLSCREEN_BUTTON_IN_PLAYER = playingPlayer != null && playingPlayer.appRequestedOverlayChildren();
+        if (FULLSCREEN_BUTTON_IN_PLAYER) {
             overlayChildren = playingPlayer.getOverlayChildren();
             overlayArea = playingPlayer.getMediaView();
         } else {
@@ -54,13 +67,13 @@ public class ModalityFullscreenButton {
             }, overlayArea.widthProperty(), overlayArea.heightProperty());
             FULLSCREEN_BUTTON.setTranslateY(-60);
         }
-        if (FULLSCREEN_BUTTON_TIMELINE != null)
-            FULLSCREEN_BUTTON_TIMELINE.stop();
+        FULLSCREEN_BUTTON_ANIMATED = false;
+        stopPreviousFullscreenButtonAnimation();
         if (FULLSCREEN_BUTTON.getTranslateY() < 0) {
             FULLSCREEN_BUTTON_TIMELINE = Animations.animateProperty(FULLSCREEN_BUTTON.translateYProperty(), 0);
-            FULLSCREEN_BUTTON_TIMELINE.setOnFinished(e -> MediaButtons.animateFullscreenButton(FULLSCREEN_BUTTON));
+            FULLSCREEN_BUTTON_TIMELINE.setOnFinished(e -> animateFullscreenButton());
         } else {
-            MediaButtons.animateFullscreenButton(FULLSCREEN_BUTTON);
+            animateFullscreenButton();
         }
         return FULLSCREEN_BUTTON;
     }
@@ -68,13 +81,51 @@ public class ModalityFullscreenButton {
     private static void hideFullscreenButton(Pane FULLSCREEN_BUTTON) {
         ObservableList<Node> overlayChildren = FXMainFrameOverlayArea.getOverlayChildren();
         if (overlayChildren.contains(FULLSCREEN_BUTTON)) {
-            if (FULLSCREEN_BUTTON_TIMELINE != null)
-                FULLSCREEN_BUTTON_TIMELINE.stop();
+            stopPreviousFullscreenButtonAnimation();
             FULLSCREEN_BUTTON_TIMELINE = Animations.animateProperty(FULLSCREEN_BUTTON.translateYProperty(), -70);
             FULLSCREEN_BUTTON_TIMELINE.setOnFinished(e -> overlayChildren.remove(FULLSCREEN_BUTTON));
         }
         if (FULLSCREEN_LAYOUT != null)
             FULLSCREEN_LAYOUT.unregister();
+    }
+
+    private static void animateFullscreenButton() {
+        animateFullscreenButton(FULLSCREEN_BUTTON);
+    }
+
+    private static void animateFullscreenButton(Pane fullscreenButton) {
+        if (!FULLSCREEN_BUTTON_IN_PLAYER)
+            MediaButtons.animateFullscreenButton(fullscreenButton);
+        else if (!FULLSCREEN_BUTTON_ANIMATED) {
+            MediaButtons.animateFullscreenButton(fullscreenButton);
+            Player player = FullscreenButton.getSuitableFullscreenPlayingPlayer();
+            Node mediaView = player.getMediaView();
+            mediaView.removeEventFilter(MouseEvent.ANY, MOUSE_MOVE_OVER_PLAYER_WITH_FULLSCREEN_BUTTON_HANDLER);
+            mediaView.addEventFilter(MouseEvent.ANY, MOUSE_MOVE_OVER_PLAYER_WITH_FULLSCREEN_BUTTON_HANDLER);
+        }
+        FULLSCREEN_BUTTON_ANIMATED = true;
+    }
+
+    private static void fadeFullscreenButton(boolean fadeIn) {
+        if (FADE_OUT_SCHEDULED != null) {
+            FADE_OUT_SCHEDULED.cancel();
+            FADE_OUT_SCHEDULED = null;
+        }
+        if (fadeIn) {
+            stopPreviousFullscreenButtonAnimation();
+            FULLSCREEN_BUTTON_TIMELINE = Animations.fade(FULLSCREEN_BUTTON, fadeIn, false);
+        } else {
+            FADE_OUT_SCHEDULED = UiScheduler.scheduleDelay(3500, () -> {
+                FULLSCREEN_BUTTON_TIMELINE = Animations.fade(FULLSCREEN_BUTTON, fadeIn, false);
+            });
+        }
+    }
+
+    private static void stopPreviousFullscreenButtonAnimation() {
+        if (FULLSCREEN_BUTTON_TIMELINE != null) {
+            FULLSCREEN_BUTTON_TIMELINE.stop();
+            FULLSCREEN_BUTTON_TIMELINE = null;
+        }
     }
 
 }
