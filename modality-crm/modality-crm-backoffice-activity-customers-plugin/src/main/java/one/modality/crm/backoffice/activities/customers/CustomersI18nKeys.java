200aab86e7047ade869c189fe7eb45392668960c
davidhello7772 <david.dev.kbs@kadampa.net>
2025-11-02 15:17:47 +0000

    Added the Customers menu Icon

---
+++ modality-crm/modality-crm-backoffice-activity-customers-plugin/src/main/java/one/modality/crm/backoffice/activities/customers/CustomersI18nKeys.java
@@ -0,0 +1,8 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+package one.modality.crm.backoffice.activities.customers;
+
+public interface CustomersI18nKeys {
+
+    Object CustomersMenu = "CustomersMenu";
+
+}
