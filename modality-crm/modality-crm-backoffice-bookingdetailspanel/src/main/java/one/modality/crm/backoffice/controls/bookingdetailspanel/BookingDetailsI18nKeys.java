f918435ca4614e80551a51488b48b5db98d5d473
david <david.dev.kbs@kadampa.net>
2025-11-03 11:16:35 +0000

    Ran webfx update

--- modality-crm/modality-crm-backoffice-bookingdetailspanel/src/main/java/one/modality/crm/backoffice/controls/bookingdetailspanel/BookingDetailsI18nKeys.java
+++ modality-crm/modality-crm-backoffice-bookingdetailspanel/src/main/java/one/modality/crm/backoffice/controls/bookingdetailspanel/BookingDetailsI18nKeys.java
@@ -3,15 +3,6 @@ package one.modality.crm.backoffice.controls.bookingdetailspanel;
 
 public interface BookingDetailsI18nKeys {
 
-    Object Options = "Options";
-    Object MultipleBookings = "MultipleBookings";
-    Object Comments = "Comments";
-    Object Mails = "Mails";
-    Object Payments = "Payments";
-    Object Family = "Family";
-    Object Medias = "Medias";
-    Object History = "History";
-    Object Cart = "Cart";
     Object PersonalDetailsTab = "PersonalDetailsTab";
     Object OptionsTab = "OptionsTab";
     Object PaymentsTab = "PaymentsTab";
@@ -22,5 +13,14 @@ public interface BookingDetailsI18nKeys {
     Object MediasTab = "MediasTab";
     Object MailsTab = "MailsTab";
     Object HistoryTab = "HistoryTab";
+    Object Options = "Options";
+    Object MultipleBookings = "MultipleBookings";
+    Object Comments = "Comments";
+    Object Mails = "Mails";
+    Object Payments = "Payments";
+    Object Family = "Family";
+    Object Medias = "Medias";
+    Object History = "History";
+    Object Cart = "Cart";
 
 }
