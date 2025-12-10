65255d89776c01c4758574570ccc9ff6c71c3e58
Bruno Salmon <dev.salmonb@gmail.com>
2025-12-10 18:14:47 +0000

    Ensured to not reply to webhook before the end of the payment update in SquareRestApiJob

--- modality-ecommerce/modality-ecommerce-payment-server-gateway-square-plugin/src/main/java/one/modality/ecommerce/payment/server/gateway/impl/square/SquareRestApiJob.java
+++ modality-ecommerce/modality-ecommerce-payment-server-gateway-square-plugin/src/main/java/one/modality/ecommerce/payment/server/gateway/impl/square/SquareRestApiJob.java
@@ -7,6 +7,8 @@ import com.squareup.square.types.Order;
 import dev.webfx.platform.ast.AST;
 import dev.webfx.platform.ast.AstObject;
 import dev.webfx.platform.ast.ReadOnlyAstObject;
+import dev.webfx.platform.async.Future;
+import dev.webfx.platform.async.Promise;
 import dev.webfx.platform.boot.spi.ApplicationJob;
 import dev.webfx.platform.console.Console;
 import dev.webfx.platform.util.Numbers;
@@ -87,28 +89,34 @@ public final class SquareRestApiJob implements ApplicationJob {
         String squareEventType = AST.lookupString(payload, "type");
         if (!"payment.updated".equals(squareEventType)) {
             Console.log(logPrefix + "⚠️  Received an event type that is not managed by this webhook: " + squareEventType);
+            ctx.response().setStatusCode(HttpResponseStatus.OK_200).end();
         } else {
             // If the payment was created through the embedded payment form, the Square payment id has been recorded
             // as transactionRef by `ServerPaymentServiceProvider.completePayment()`. So we can find it by this id.
             String transactionRef = AST.lookupString(payload, "data.object.payment.id");
-            if (transactionRef == null)
+            if (transactionRef == null) {
                 Console.log(logPrefix + "⛔️  Couldn't find data.object.payment.id in the payload!");
-            else
-                loadAndUpdatePaymentStatus("transactionRef", transactionRef, live, payload, textPayload, logPrefix);
+                ctx.response().setStatusCode(HttpResponseStatus.BAD_REQUEST_400).end();
+            } else
+                loadAndUpdatePaymentStatus("transactionRef", transactionRef, live, payload, textPayload, logPrefix)
+                    .onSuccess(v -> ctx.response().setStatusCode(HttpResponseStatus.OK_200).end())
+                    .onFailure(ex -> {
+                        Console.log(logPrefix + "⛔️️  An error occurred while processing the Square webhook", ex);
+                        ctx.response().setStatusCode(HttpResponseStatus.INTERNAL_SERVER_ERROR_500).end();
+                    });
         }
-        ctx.response().setStatusCode(HttpResponseStatus.OK_200).end();
     }
 
-    private static void loadAndUpdatePaymentStatus(String field, Object value, boolean live, AstObject payload, String textPayload, String logPrefix) {
-        EntityStore.create()
+    private static Future<Void> loadAndUpdatePaymentStatus(String field, Object value, boolean live, AstObject payload, String textPayload, String logPrefix) {
+        return EntityStore.create()
             .<MoneyTransfer>executeQuery("select pending,successful,status,gatewayResponse from MoneyTransfer where " + field + " = ?", value)
-            .onFailure(e -> Console.log(logPrefix + "⛔️️  An error occurred when reading the payment with " + field + " = " + value, e))
-            .onSuccess(payments -> {
+            //.onFailure(e -> Console.log(logPrefix + "⛔️️  An error occurred when reading the payment with " + field + " = " + value, e))
+            .compose(payments -> {
                 int n = payments.size();
                 if (n == 1)
-                    updatePaymentStatus(payments.get(0), payload, textPayload, logPrefix);
+                    return updatePaymentStatus(payments.get(0), payload, textPayload, logPrefix);
                 else if (n != 0 || "id".equals(field)) {
-                    Console.log(logPrefix + "⛔️️  " + payments.size() + " payments were found in the database with " + field + " = " + value + " !");
+                    return Future.failedFuture(payments.size() + " payments were found in the database with " + field + " = " + value + " !");
                 } else {
                     // We didn't find it, but this can happen with redirected payments because completePayment()
                     // has not been called for them. The way to find a redirected payment is to retrieve its
@@ -117,18 +125,19 @@ public final class SquareRestApiJob implements ApplicationJob {
                     // Modality account parameters.
                     String merchantId = payload.getString("merchant_id");
                     String orderId = AST.lookupString(payload, "data.object.payment.order_id");
-                    if (merchantId == null)
-                        Console.log(logPrefix + "⛔️️  No merchant_id was found in the payload!");
-                    else if (orderId == null)
-                        Console.log(logPrefix + "⛔️️  No order_id was found in the payload!");
-                    else {
-                        payments.getStore().<GatewayParameter>executeQuery("select value from GatewayParameter p where account.gatewayCompany.name='Square' and name='access_token' and live=$1 and exists(select GatewayParameter where account=p.account and name='merchant_id' and value=$2) order by id desc limit 1", live, merchantId)
-                            .onFailure(e -> Console.log(logPrefix + "⛔️️  An error occurred when reading the account parameters", e))
-                            .onSuccess(parameters -> {
+                    if (merchantId == null) {
+                        return Future.failedFuture("No merchant_id was found in the payload!");
+                    } else if (orderId == null) {
+                        return Future.failedFuture("No order_id was found in the payload!");
+                    } else {
+                        return payments.getStore().<GatewayParameter>executeQuery("select value from GatewayParameter p where account.gatewayCompany.name='Square' and name='access_token' and live=$1 and exists(select GatewayParameter where account=p.account and name='merchant_id' and value=$2) order by id desc limit 1", live, merchantId)
+                            //.onFailure(e -> Console.log(logPrefix + "⛔️️  An error occurred when reading the account parameters", e))
+                            .compose(parameters -> {
                                 GatewayParameter accessTokenParameter = Collections.first(parameters);
                                 if (accessTokenParameter == null)
-                                    Console.log(logPrefix + "⛔️️  No access token was found in the database for the Square account with merchant_id = " + merchantId + " and live = " + live);
+                                    return Future.failedFuture("No access token was found in the database for the Square account with merchant_id = " + merchantId + " and live = " + live);
                                 else {
+                                    Promise<Void> promise = Promise.promise();
                                     String accessToken = accessTokenParameter.getValue();
                                     AsyncSquareClient client = AsyncSquareClient.builder()
                                         .environment(live ? Environment.PRODUCTION : Environment.SANDBOX)
@@ -146,9 +155,13 @@ public final class SquareRestApiJob implements ApplicationJob {
                                             if (referenceId == null)
                                                 Console.log(logPrefix + "⛔️️  No referenceId was found in the Square API for orderId = " + orderId);
                                             else
-                                                loadAndUpdatePaymentStatus("id", Numbers.toShortestNumber(referenceId), live, payload, textPayload, logPrefix);
+                                                promise.handle(loadAndUpdatePaymentStatus("id", Numbers.toShortestNumber(referenceId), live, payload, textPayload, logPrefix));
                                         }
+                                    }).exceptionally(e -> {
+                                        promise.tryFail(e);
+                                        return null;
                                     });
+                                    return promise.future();
                                 }
                             });
                     }
@@ -156,7 +169,7 @@ public final class SquareRestApiJob implements ApplicationJob {
             });
     }
 
-    private static void updatePaymentStatus(MoneyTransfer payment, AstObject payload, String textPayload, String logPrefix) {
+    private static Future<Void> updatePaymentStatus(MoneyTransfer payment, AstObject payload, String textPayload, String logPrefix) {
         ReadOnlyAstObject paymentObject = AST.lookupObject(payload, "data.object.payment");
         String id = paymentObject.getString("id"); // Corresponds to transactionRef in Modality
         String status = paymentObject.getString("status");
@@ -173,20 +186,22 @@ public final class SquareRestApiJob implements ApplicationJob {
             ReadOnlyAstObject dbPaymentObject = AST.lookupObject(dbPayload, "data.object.payment");
             if (dbPaymentObject != null && dbPaymentObject.getInteger("version") >= paymentObject.getInteger("version")) {
                 Console.log(logPrefix + "Skipping this event because it's not newer compared to the database");
+                return Future.succeededFuture();
             } else {
                 // We do a simple update of the payment gateway response without creating a new history entry
                 UpdateStore updateStore = UpdateStore.createAbove(payment.getStore());
                 payment = updateStore.updateEntity(payment);
                 payment.setGatewayResponse(textPayload);
-                updateStore.submitChanges()
-                    .onFailure(e -> Console.log(logPrefix + "⛔️️  Failed to update gatewayResponse for payment " + paymentPk, e))
-                    .onSuccess(v -> Console.log(logPrefix + "✅  Successfully updated gatewayResponse for payment " + paymentPk));
+                return updateStore.submitChanges()
+                    //.onFailure(e -> Console.log(logPrefix + "⛔️️  Failed to update gatewayResponse for payment " + paymentPk, e))
+                    .onSuccess(v -> Console.log(logPrefix + "✅  Successfully updated gatewayResponse for payment " + paymentPk))
+                    .mapEmpty();
             }
         } else {
             // We finally update the payment status through the payment service (this will also create a history entry)
-            SQUARE_HISTORY_USER_ID.callAndReturn(() ->
+            return SQUARE_HISTORY_USER_ID.callAndReturn(() ->
                 PaymentService.updatePaymentStatus(UpdatePaymentStatusArgument.createCapturedStatusArgument(paymentPk, textPayload, id, status, pending, successful))
-                    .onFailure(e -> Console.log(logPrefix + "⛔️️  Failed to update status " + status + " for transactionRef = " + id, e))
+                    //.onFailure(e -> Console.log(logPrefix + "⛔️️  Failed to update status " + status + " for transactionRef = " + id, e))
                     .onSuccess(v -> Console.log(logPrefix + "✅  Successfully updated status " + status + " for transactionRef = " + id))
             );
         }
