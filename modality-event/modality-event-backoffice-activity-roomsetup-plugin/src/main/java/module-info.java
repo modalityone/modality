f7e68570361ddbaaa6027a3295ae400c8c2dae65
david <david.dev.kbs@kadampa.net>
2026-01-06 14:00:01 +0000

    Added first version of EventRoomSetupActivity

--- modality-event/modality-event-backoffice-activity-roomsetup-plugin/src/main/java/module-info.java
+++ modality-event/modality-event-backoffice-activity-roomsetup-plugin/src/main/java/module-info.java
@@ -3,13 +3,36 @@
 module modality.event.backoffice.activity.roomsetup.plugin {
 
     // Direct dependencies modules
+    requires javafx.base;
+    requires javafx.controls;
     requires javafx.graphics;
+    requires modality.base.backoffice.mainframe.fx;
     requires modality.base.client.activity;
+    requires modality.base.client.bootstrap;
+    requires modality.base.client.mainframe.fx;
+    requires modality.base.client.tile;
+    requires modality.base.shared.entities;
+    requires modality.crm.backoffice.organization.fx;
+    requires modality.event.client.event.fx;
+    requires webfx.extras.controlfactory;
     requires webfx.extras.i18n;
+    requires webfx.extras.i18n.controls;
     requires webfx.extras.operation;
     requires webfx.extras.operation.action;
+    requires webfx.extras.styles.bootstrap;
+    requires webfx.extras.theme;
+    requires webfx.extras.util.control;
+    requires webfx.extras.util.dialog;
+    requires webfx.kit.util;
+    requires webfx.platform.console;
     requires webfx.platform.windowhistory;
+    requires webfx.stack.orm.domainmodel;
     requires webfx.stack.orm.domainmodel.activity;
+    requires webfx.stack.orm.dql;
+    requires webfx.stack.orm.entity;
+    requires webfx.stack.orm.entity.controls;
+    requires webfx.stack.orm.reactive.entities;
+    requires webfx.stack.routing.activity;
     requires webfx.stack.routing.router;
     requires webfx.stack.routing.router.client;
     requires webfx.stack.routing.uirouter;
