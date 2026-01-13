0ced76504eeb2fcfc867bf03fef75dd178450963
Bruno Salmon <dev.salmonb@gmail.com>
2026-01-13 12:22:57 +0000

    Added getPhases() and setPhases() in PhaseCoverage (implemented in PhaseCoverageImpl) - also added phase4 field and made PhaseCoverage implement BoundaryPeriod

--- modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/PhaseCoverage.java
+++ modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/PhaseCoverage.java
@@ -1,10 +1,13 @@
 package one.modality.base.shared.entities;
 
+import dev.webfx.platform.util.collection.Collections;
 import dev.webfx.stack.orm.entity.Entity;
 import dev.webfx.stack.orm.entity.EntityId;
 import one.modality.base.shared.entities.markers.EntityHasEvent;
 import one.modality.base.shared.entities.markers.EntityHasLabel;
 import one.modality.base.shared.entities.markers.EntityHasName;
+
+import java.util.List;
 
 /**
  * @author Bruno Salmon
@@ -12,11 +15,13 @@ import one.modality.base.shared.entities.markers.EntityHasName;
 public interface PhaseCoverage extends Entity,
     EntityHasEvent,
     EntityHasName,
-    EntityHasLabel
+    EntityHasLabel,
+    BoundaryPeriod // Can be used as a BoundaryPeriod when all phases are consecutive (no discontinuity)
 {
     String phase1 = "phase1";
     String phase2 = "phase2";
     String phase3 = "phase3";
+    String phase4 = "phase4";
 
     default void setPhase1(Object value) {
         setForeignField(phase1, value);
@@ -52,6 +57,32 @@ public interface PhaseCoverage extends Entity,
 
     default EventPhase getPhase3() {
         return getForeignEntity(phase3);
+    }
+
+    default void setPhase4(Object value) {
+        setForeignField(phase4, value);
+    }
+
+    default EntityId getPhase4Id() {
+        return getForeignEntityId(phase4);
+    }
+
+    default EventPhase getPhase4() {
+        return getForeignEntity(phase4);
+    }
+
+    List<EventPhase> getPhases(); // implemented in PhaseCoverageImpl
+
+    void setPhases(List<EventPhase> phases); // implemented in PhaseCoverageImpl
+
+    @Override
+    default ScheduledBoundary getStartBoundary() {
+        return getPhase1().getStartBoundary();
+    }
+
+    @Override
+    default ScheduledBoundary getEndBoundary() {
+        return Collections.last(getPhases()).getEndBoundary();
     }
 
 }
--- modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/impl/PhaseCoverageImpl.java
+++ modality-base/modality-base-shared-entities/src/main/java/one/modality/base/shared/entities/impl/PhaseCoverageImpl.java
@@ -1,18 +1,40 @@
 package one.modality.base.shared.entities.impl;
 
+import dev.webfx.platform.util.collection.Collections;
 import dev.webfx.stack.orm.entity.EntityId;
 import dev.webfx.stack.orm.entity.EntityStore;
 import dev.webfx.stack.orm.entity.impl.DynamicEntity;
 import dev.webfx.stack.orm.entity.impl.EntityFactoryProviderImpl;
+import one.modality.base.shared.entities.EventPhase;
 import one.modality.base.shared.entities.PhaseCoverage;
+
+import java.util.List;
 
 /**
  * @author Bruno Salmon
  */
 public final class PhaseCoverageImpl extends DynamicEntity implements PhaseCoverage {
 
+    private List<EventPhase> phases;
+
     public PhaseCoverageImpl(EntityId id, EntityStore store) {
         super(id, store);
+    }
+
+    @Override
+    public List<EventPhase> getPhases() {
+        if (phases == null)
+            phases = Collections.removeNulls(Collections.listOf(getPhase1(), getPhase2(), getPhase3(), getPhase4()));
+        return phases;
+    }
+
+    @Override
+    public void setPhases(List<EventPhase> phases) {
+        this.phases = phases;
+        setPhase1(Collections.get(phases, 0));
+        setPhase2(Collections.get(phases, 1));
+        setPhase3(Collections.get(phases, 2));
+        setPhase4(Collections.get(phases, 3));
     }
 
     public static final class ProvidedFactory extends EntityFactoryProviderImpl<PhaseCoverage> {
