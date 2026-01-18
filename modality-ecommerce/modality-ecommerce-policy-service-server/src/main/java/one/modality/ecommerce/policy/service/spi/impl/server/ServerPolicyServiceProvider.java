821bacae07766ca6ef29ba66c20b878dc9b9a759
Bruno Salmon <dev.salmonb@gmail.com>
2026-01-18 15:30:17 +0000

    Used PoolAllocation quantity to determine the max available space for resources in ServerPolicyServiceProvider

--- modality-ecommerce/modality-ecommerce-policy-service-server/src/main/java/one/modality/ecommerce/policy/service/spi/impl/server/ServerPolicyServiceProvider.java
+++ modality-ecommerce/modality-ecommerce-policy-service-server/src/main/java/one/modality/ecommerce/policy/service/spi/impl/server/ServerPolicyServiceProvider.java
@@ -21,8 +21,10 @@ public final class ServerPolicyServiceProvider implements PolicyServiceProvider 
     private final static String SCHEDULED_ITEMS_QUERY_BASE =
         "select name,label,site.name,item.(name,label,code,family.(code,name,label),capacity,share_mate,ord),date,startTime,timeline.(site,item,startTime,endTime),cancelled,resource" +
         // We also compute the remaining available space for guests
-        ",(select sum(coalesce(max,configuration.max) - coalesce((select sum(documentLine.quantity) from Attendance where scheduledResource=sr and present and !documentLine.frontend_released), 0))" +
-            " from ScheduledResource sr" +
+        ",(select sum(" +
+            "coalesce((select quantity from PoolAllocation where resource=sr.configuration.resource and pool= " + GENERAL_GUESTS_EVENT_POOL_ID + " and event=$1 limit 1), max, configuration.max)" +
+            " - coalesce((select sum(documentLine.quantity) from Attendance where scheduledResource=sr and present and documentLine.(!frontend_released and poolAllocation.(resource=sr.configuration.resource and pool= " + GENERAL_GUESTS_EVENT_POOL_ID + "))), 0)" +
+            ") from ScheduledResource sr" +
             // We consider only the resources allocated to the general guest pool for this event
             " where scheduledItem=si and exists(select PoolAllocation where resource=sr.configuration.resource and pool= " + GENERAL_GUESTS_EVENT_POOL_ID + " and event=$1)" +
             " group by scheduledItem)" +
